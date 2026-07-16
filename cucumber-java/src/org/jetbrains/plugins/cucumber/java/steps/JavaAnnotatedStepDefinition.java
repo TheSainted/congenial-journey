697a67816300e4e7a97148db9028fe7aad0ab86f
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:13:54 +0000

    Initialize nkatkov/jpa-uast-inspections

---
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/steps/JavaAnnotatedStepDefinition.java
@@ -0,0 +1,33 @@
+package org.jetbrains.plugins.cucumber.java.steps;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.cucumber.java.CucumberJavaUtil;
+
+public class JavaAnnotatedStepDefinition extends AbstractJavaStepDefinition {
+  private final String myAnnotationClassName;
+
+  public JavaAnnotatedStepDefinition(@NotNull PsiElement stepDef, @NotNull String annotationClassName) {
+    super(stepDef);
+    myAnnotationClassName = annotationClassName;
+  }
+
+  @Nullable
+  @Override
+  protected String getCucumberRegexFromElement(PsiElement element) {
+    if (element == null) {
+      return null;
+    }
+
+    if (!(element instanceof PsiMethod)) {
+      return null;
+    }
+    String patternText = CucumberJavaUtil.getStepAnnotationValue((PsiMethod)element, myAnnotationClassName);
+    if (patternText != null && patternText.length() > 1) {
+      return patternText.replace("\\\\", "\\").replace("\\\"", "\"");
+    }
+    return null;
+  }
+}
