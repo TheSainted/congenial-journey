ae670c351ffcbb1e06381217482dacbe5a2f4f30
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 21:03:24 -0700

    Initialize alobov/ws-nicer-type-checking-errors

---
+++ AngularJS/src/org/angularjs/codeInsight/AngularJSAnalysisHandlersFactory.java
@@ -0,0 +1,111 @@
+package org.angularjs.codeInsight;
+
+import com.intellij.codeInsight.template.Template;
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.lang.javascript.JSAnalysisHandlersFactory;
+import com.intellij.lang.javascript.psi.JSCallExpression;
+import com.intellij.lang.javascript.psi.JSReferenceExpression;
+import com.intellij.lang.javascript.psi.ecmal4.JSClass;
+import com.intellij.lang.javascript.psi.util.JSClassUtils;
+import com.intellij.lang.javascript.validation.JSReferenceChecker;
+import com.intellij.lang.javascript.validation.JSReferenceInspectionProblemReporter;
+import com.intellij.lang.javascript.validation.fixes.CreateJSFunctionIntentionAction;
+import com.intellij.lang.javascript.validation.fixes.CreateJSVariableIntentionAction;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.ResolveResult;
+import org.angularjs.index.AngularJS2IndexingHandler;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class AngularJSAnalysisHandlersFactory extends JSAnalysisHandlersFactory {
+  @NotNull
+  @Override
+  public JSReferenceChecker getReferenceChecker(@NotNull JSReferenceInspectionProblemReporter reporter) {
+    return new JSReferenceChecker(reporter) {
+      @Override
+      protected void addCreateFromUsageFixesForCall(@NotNull JSCallExpression node,
+                                                    @NotNull JSReferenceExpression referenceExpression,
+                                                    @NotNull ResolveResult[] resolveResults,
+                                                    @NotNull List<LocalQuickFix> quickFixes) {
+        if (referenceExpression.getQualifier() != null) return;
+
+        JSClass directive = AngularJS2IndexingHandler.findDirectiveClass(node);
+        if (directive != null) {
+          quickFixes.add(new CreateJSFunctionIntentionAction(referenceExpression.getReferencedName(), true) {
+            @Override
+            protected void applyFix(Project project, PsiElement psiElement, PsiFile file, Editor editor) {
+              JSClass directive = AngularJS2IndexingHandler.findDirectiveClass(psiElement);
+              assert directive != null;
+              doApplyFix(project, directive, directive.getContainingFile(), null);
+            }
+
+            @NotNull
+            @Override
+            protected Pair<JSReferenceExpression, PsiElement> calculateAnchors(PsiElement psiElement) {
+              return Pair.create(referenceExpression, psiElement.getLastChild());
+            }
+
+            @Override
+            protected void writeFunctionAndName(Template template,
+                                                String createdMethodName,
+                                                PsiFile file,
+                                                PsiElement clazz,
+                                                JSReferenceExpression referenceExpression) {
+              template.addTextSegment(JSClassUtils.createClassFunctionName(createdMethodName, file));
+            }
+            
+            @Override
+            protected void addAccessModifier(Template template,
+                                             JSReferenceExpression referenceExpression,
+                                             PsiFile file,
+                                             boolean staticContext,
+                                             JSClass contextClass) {
+            }
+          });
+        }
+      }
+
+      @Override
+      protected boolean addCreateFromUsageFixes(JSReferenceExpression referenceExpression,
+                                                ResolveResult[] resolveResults,
+                                                List<LocalQuickFix> quickFixes,
+                                                boolean inTypeContext,
+                                                boolean ecma) {
+        if (referenceExpression.getQualifier() != null) return inTypeContext;
+
+        JSClass directive = AngularJS2IndexingHandler.findDirectiveClass(referenceExpression);
+        if (directive != null) {
+          quickFixes.add(new CreateJSVariableIntentionAction(referenceExpression.getReferencedName(), true, false, null) {
+            @Override
+            protected void applyFix(Project project, PsiElement psiElement, PsiFile file, Editor editor) {
+              JSClass directive = AngularJS2IndexingHandler.findDirectiveClass(psiElement);
+              assert directive != null;
+              doApplyFix(project, directive, directive.getContainingFile(), null);
+            }
+
+            @NotNull
+            @Override
+            protected Pair<JSReferenceExpression, PsiElement> calculateAnchors(PsiElement psiElement) {
+              return Pair.create(referenceExpression, psiElement.getLastChild());
+            }
+
+            @Override
+            protected void addAccessModifier(Template template,
+                                             JSReferenceExpression referenceExpression,
+                                             PsiFile file,
+                                             boolean staticContext,
+                                             JSClass contextClass) {
+            }
+          });
+        }
+
+        return inTypeContext;
+      }
+    };
+  }
+}
