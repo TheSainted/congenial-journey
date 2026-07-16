0f403568dc7a98317fee5ca2a193733ab57e61a3
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:01:47 -0700

    Initialize 162.1889

---
+++ Dart/src/com/jetbrains/lang/dart/ide/actions/DartPubUpgradeAction.java
@@ -0,0 +1,23 @@
+package com.jetbrains.lang.dart.ide.actions;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.jetbrains.lang.dart.DartBundle;
+import com.jetbrains.lang.dart.util.PubspecYamlUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class DartPubUpgradeAction extends DartPubActionBase {
+  @Override
+  @NotNull
+  protected String getTitle(@NotNull final VirtualFile pubspecYamlFile) {
+    final String projectName = PubspecYamlUtil.getDartProjectName(pubspecYamlFile);
+    final String prefix = projectName == null ? "" : ("[" + projectName + "] ");
+    return prefix + DartBundle.message("dart.pub.upgrade.title");
+  }
+
+  @Nullable
+  protected String[] calculatePubParameters(@NotNull final Project project, @NotNull final VirtualFile pubspecYamlFile) {
+    return new String[]{"upgrade"};
+  }
+}
