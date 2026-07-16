8255e1abc86844011854d5a3045bfdacfe7c3638
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:33:39 +0000

    Initialize link/PY-32773

---
+++ flex/flex-tests/testSrc/com/intellij/flex/highlighting/GlobalFlexHighlightingTest.java
@@ -0,0 +1,91 @@
+package com.intellij.flex.highlighting;
+
+import com.intellij.codeInsight.daemon.impl.DefaultHighlightVisitorBasedInspection;
+import com.intellij.codeInspection.ex.InspectionToolRegistrar;
+import com.intellij.codeInspection.ex.InspectionToolWrapper;
+import com.intellij.flex.util.FlexTestUtils;
+import com.intellij.lang.javascript.JSTestOption;
+import com.intellij.lang.javascript.JSTestUtils;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.testFramework.InspectionTestCase;
+
+import java.util.List;
+
+public class GlobalFlexHighlightingTest extends InspectionTestCase {
+  @Override
+  protected void setUp() throws Exception {
+    FlexTestUtils.allowFlexVfsRootsFor(getTestRootDisposable(), "global_inspections");
+    super.setUp();
+  }
+
+  @Override
+  protected Sdk getTestProjectJdk() {
+    return getTestProjectSdk();
+  }
+
+  @Override
+  protected Sdk getTestProjectSdk() {
+    final JSTestUtils.TestDescriptor testDescriptor = new JSTestUtils.TestDescriptor(this);
+    if (JSTestUtils.testMethodHasOption(testDescriptor, JSTestOption.WithFlexSdk)) {
+      return FlexTestUtils.getSdk(testDescriptor, getTestRootDisposable());
+    }
+    return super.getTestProjectSdk();
+  }
+
+  public void testAvailability() {
+    boolean foundSyntaxCheckInspection = false;
+    boolean foundAnnotatorInspection = false;
+
+    List<InspectionToolWrapper> tools = InspectionToolRegistrar.getInstance().createTools();
+    for (InspectionToolWrapper tool : tools) {
+      String shortName = tool.getShortName();
+      foundAnnotatorInspection |= shortName.equals("Annotator");
+      foundSyntaxCheckInspection |= shortName.equals("SyntaxError");
+    }
+
+    assertTrue("Should have global syntax inspection provided", foundSyntaxCheckInspection);
+    assertTrue("Should have global annotator inspection provided", foundAnnotatorInspection);
+  }
+
+  public void testReportingSyntaxProblemsInMxml() {
+    doSyntaxErrorsTest();
+  }
+
+  public void testReportingSyntaxProblemsInActionScript() {
+    doSyntaxErrorsTest();
+  }
+
+  private void doSyntaxErrorsTest() {
+    doTest(getTestName(false), new DefaultHighlightVisitorBasedInspection.SyntaxErrorInspection());
+  }
+
+/*
+  @JSTestOptions({JSTestOption.WithFlexSdk})
+  public void testReportingAnnotatorProblemsInMxml() {
+    doAnnotatorTest();
+  }
+
+  public void testReportingAnnotatorProblemsInActionScript() {
+    doAnnotatorTest();
+  }
+
+
+  private void doAnnotatorTest() {
+    final InspectionProfileImpl profile = InspectionProjectProfileManager.getInstance(myProject).getCurrentProfile();
+    InspectionsKt.runInInitMode(() -> {
+      profile.enableTool(JSUnresolvedVariableInspection.SHORT_NAME, myProject);
+      return null;
+    });
+    try {
+      doTest(getTestName(false), new DefaultHighlightVisitorBasedInspection.AnnotatorBasedInspection());
+    }
+    finally {
+      profile.setToolEnabled(JSUnresolvedVariableInspection.SHORT_NAME, false);
+    }
+  }
+*/
+  @Override
+  public String getTestDataPath() {
+    return FlexTestUtils.getTestDataPath("global_inspections");
+  }
+}
