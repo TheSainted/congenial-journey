9556513e5860ffb2e2cf4919df688c6ea71135cb
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:17:35 +0000

    Initialize slava/runconfigs_refactoring

---
+++ AngularJS/src/org/angularjs/findUsages/AngularJSFindUsagesHandlerFactory.java
@@ -0,0 +1,58 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.angularjs.findUsages;
+
+import com.intellij.find.findUsages.FindUsagesHandler;
+import com.intellij.lang.javascript.findUsages.JavaScriptFindUsagesHandlerFactory;
+import com.intellij.lang.javascript.psi.JSCallExpression;
+import com.intellij.lang.javascript.psi.ecma6.ES6Decorator;
+import com.intellij.lang.javascript.psi.ecmal4.JSAttributeList;
+import com.intellij.lang.javascript.psi.ecmal4.JSClass;
+import com.intellij.lang.javascript.psi.stubs.JSElementIndexingData;
+import com.intellij.lang.javascript.psi.stubs.JSImplicitElement;
+import com.intellij.psi.PsiElement;
+import org.angular2.codeInsight.Angular2PipeUtil;
+import org.angular2.codeInsight.metadata.AngularPipeMetadata;
+import org.angular2.lang.Angular2LangUtil;
+import org.angularjs.codeInsight.DirectiveUtil;
+import org.jetbrains.annotations.NotNull;
+
+public class AngularJSFindUsagesHandlerFactory extends JavaScriptFindUsagesHandlerFactory {
+
+  @Override
+  public boolean canFindUsages(@NotNull PsiElement element) {
+    return DirectiveUtil.getDirective(element) != null
+           || element instanceof JSClass
+           || Angular2PipeUtil.getPipe(element) != null;
+  }
+
+  @Override
+  public FindUsagesHandler createFindUsagesHandler(@NotNull PsiElement element, boolean forHighlightUsages) {
+    final JSImplicitElement pipe;
+    if (!forHighlightUsages && (pipe = Angular2PipeUtil.getPipe(element)) != null) {
+      JSClass cls = AngularPipeMetadata.create(pipe).getPipeClass();
+      return new JavaScriptFindUsagesHandlerFactory.JavaScriptFindUsagesHandler(
+        element != cls ? pipe : cls, element == cls ? new PsiElement[]{pipe} : PsiElement.EMPTY_ARRAY);
+    }
+
+    if (!forHighlightUsages && element instanceof JSClass && Angular2LangUtil.isAngular2Context(element)) {
+      return new JavaScriptFindUsagesHandlerFactory.JavaScriptFindUsagesHandler(element) {
+        @NotNull
+        @Override
+        public PsiElement[] getSecondaryElements() {
+          JSAttributeList list = ((JSClass)element).getAttributeList();
+          if (list != null && list.getFirstChild() instanceof ES6Decorator) {
+            PsiElement call = list.getFirstChild().getLastChild();
+            if (call instanceof JSCallExpression) {
+              JSElementIndexingData data = ((JSCallExpression)call).getIndexingData();
+              if (data != null && data.getImplicitElements() != null) {
+                return data.getImplicitElements().toArray(PsiElement.EMPTY_ARRAY);
+              }
+            }
+          }
+          return PsiElement.EMPTY_ARRAY;
+        }
+      };
+    }
+    return super.createFindUsagesHandler(element, forHighlightUsages);
+  }
+}
---
+++ prettierJS/src/com/intellij/prettierjs/PrettierConfiguration.java
@@ -0,0 +1,132 @@
+package com.intellij.prettierjs;
+
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.javascript.nodejs.PackageJsonData;
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterRef;
+import com.intellij.javascript.nodejs.util.NodePackage;
+import com.intellij.lang.javascript.buildTools.npm.PackageJsonUtil;
+import com.intellij.lang.javascript.modules.NodeModuleUtil;
+import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.DumbAwareRunnable;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupManager;
+import com.intellij.openapi.util.Ref;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.openapi.vfs.newvfs.BulkFileListener;
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
+import com.intellij.platform.DirectoryProjectConfigurator;
+import com.intellij.util.ObjectUtils;
+import com.intellij.util.Producer;
+import com.intellij.util.concurrency.SequentialTaskExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+public class PrettierConfiguration {
+  @NotNull
+  private final Project myProject;
+  private final PropertiesComponent myPropertiesComponent;
+  private static final String NODE_INTERPRETER_PROPERTY = "prettierjs.PrettierConfiguration.NodeInterpreter";
+  private static final String PACKAGE_PROPERTY = "prettierjs.PrettierConfiguration.Package";
+  private static final String OLD_PACKAGE_PROPERTY = "node.js.selected.package.prettier";
+  private static final String OLD_INTERPRETER_PROPERTY = "node.js.path.for.package.prettier";
+
+  public PrettierConfiguration(@NotNull Project project, @NotNull PropertiesComponent component) {
+    myProject = project;
+    myPropertiesComponent = component;
+    ExecutorService threadPoolExecutor = SequentialTaskExecutor.createSequentialApplicationPoolExecutor("Prettier.PackageJsonUpdater");
+    project.getMessageBus().connect().subscribe(VirtualFileManager.VFS_CHANGES, new BulkFileListener() {
+      @Override
+      public void after(@NotNull List<? extends VFileEvent> events) {
+        for (VFileEvent event : events) {
+          if (PackageJsonUtil.isPackageJsonFile(event.getFile())) {
+            threadPoolExecutor.submit(() -> ReadAction.run(PrettierConfiguration.this::detectLocalPackage));
+          }
+        }
+      }
+    });
+  }
+
+  @NotNull
+  public static PrettierConfiguration getInstance(@NotNull Project project) {
+    return ServiceManager.getService(project, PrettierConfiguration.class);
+  }
+
+  @NotNull
+  public NodeJsInterpreterRef getInterpreterRef() {
+    return NodeJsInterpreterRef.create(ObjectUtils.coalesce(myPropertiesComponent.getValue(NODE_INTERPRETER_PROPERTY),
+                                                            myPropertiesComponent.getValue(OLD_INTERPRETER_PROPERTY)));
+  }
+
+  @Nullable
+  public NodePackage getPackage() {
+    String value = ObjectUtils.coalesce(myPropertiesComponent.getValue(PACKAGE_PROPERTY),
+                                        myPropertiesComponent.getValue(OLD_PACKAGE_PROPERTY),
+                                        "");
+    return new NodePackage(value);
+  }
+
+  public void update(@NotNull NodeJsInterpreterRef interpreterRef, @Nullable NodePackage nodePackage) {
+    myPropertiesComponent.setValue(NODE_INTERPRETER_PROPERTY, interpreterRef.getReferenceName());
+    myPropertiesComponent.setValue(PACKAGE_PROPERTY, nodePackage != null ? nodePackage.getSystemDependentPath() : null);
+  }
+
+  private void detectLocalOrGlobalPackage() {
+    detectPackage(() -> ObjectUtils.coalesce(localPackageIfInDependencies(), findDefaultPackage()));
+  }
+
+  private void detectLocalPackage() {
+    detectPackage(() -> localPackageIfInDependencies());
+  }
+
+  private void detectPackage(@NotNull Producer<NodePackage> packageProducer) {
+    if (myProject.isDisposed() || myProject.isDefault()) {
+      return;
+    }
+    String stored = myPropertiesComponent.getValue(PACKAGE_PROPERTY, "");
+    if (!StringUtil.isEmpty(stored)) {
+      return;
+    }
+
+    NodePackage detected = packageProducer.produce();
+    if (detected != null) {
+      myPropertiesComponent.setValue(PACKAGE_PROPERTY, detected.getSystemDependentPath());
+    }
+  }
+
+  @Nullable
+  private NodePackage localPackageIfInDependencies() {
+    if (myProject.isDefault() || myProject.getBasePath() == null) {
+      return null;
+    }
+    final PackageJsonData data = PackageJsonUtil.getTopLevelPackageJsonData(myProject);
+    if (data != null && data.isDependencyOfAnyType(PrettierUtil.PACKAGE_NAME)) {
+      final String basePath = FileUtil.toSystemDependentName(myProject.getBasePath());
+      return new NodePackage((basePath.endsWith(File.separator) ? basePath : (basePath + File.separator))
+                             + NodeModuleUtil.NODE_MODULES + File.separator
+                             + PrettierUtil.PACKAGE_NAME);
+    }
+    return null;
+  }
+
+  @Nullable
+  private NodePackage findDefaultPackage() {
+    return NodePackage.findDefaultPackage(myProject, PrettierUtil.PACKAGE_NAME, getInterpreterRef().resolve(myProject));
+  }
+
+  public static class ProjectConfigurator implements DirectoryProjectConfigurator {
+    @Override
+    public void configureProject(Project project, @NotNull VirtualFile baseDir, Ref<Module> moduleRef) {
+      StartupManager.getInstance(project).runWhenProjectIsInitialized(
+        (DumbAwareRunnable)() -> getInstance(project).detectLocalOrGlobalPackage());
+    }
+  }
+}
