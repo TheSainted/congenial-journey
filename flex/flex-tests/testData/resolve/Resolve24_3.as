0062b502197f9ef0ee78c4c54642f893c28622a8
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:11:22 +0000

    Initialize ifedorov/batch_inspection_with_coverage

---
+++ flex/flex-tests/testData/resolve/Resolve24_3.as
@@ -0,0 +1,6 @@
+import xxx.A
+z.aaa = 1;
+
+function fff() {
+  A.a<caret>aa = 1;
+}
---
+++ vuejs/src/org/jetbrains/vuejs/index/VueIndexBase.kt
@@ -0,0 +1,31 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.vuejs.index
+
+import com.intellij.lang.javascript.psi.JSImplicitElementProvider
+import com.intellij.lang.javascript.psi.stubs.impl.JSImplicitElementImpl
+import com.intellij.psi.stubs.StringStubIndexExtension
+import com.intellij.psi.stubs.StubIndexKey
+
+/**
+ * @author Irina.Chernushina on 7/19/2017.
+ */
+abstract class VueIndexBase(private val key: StubIndexKey<String, JSImplicitElementProvider>,
+                            jsKey: String) : StringStubIndexExtension<JSImplicitElementProvider>() {
+  private val VERSION = 25
+
+  init {
+    // this is called on index==application component initialization
+    JSImplicitElementImpl.ourUserStringsRegistry.registerUserString(jsKey)
+  }
+
+  companion object {
+    fun createJSKey(key: StubIndexKey<String, JSImplicitElementProvider>): String =
+      key.name.split(".").joinToString("") { it.subSequence(0, 1) }
+  }
+
+  override fun getKey(): StubIndexKey<String, JSImplicitElementProvider> = key
+
+  override fun getVersion(): Int {
+    return VERSION
+  }
+}
