b5b58c4818d4a5d53c552fe57163deeae68fdba7
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:25:46 +0000

    Initialize ostrizhe/sort_imports

---
+++ Dart/gen/icons/DartIcons.java
@@ -0,0 +1,29 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class DartIcons {
+  private static Icon load(String path) {
+    return IconLoader.getIcon(path, DartIcons.class);
+  }
+
+  public static final Icon Dart_13 = load("/icons/dart_13.svg"); // 13x13
+  public static final Icon Dart_16 = load("/icons/dart_16.svg"); // 16x16
+  public static final Icon Dart_file = load("/icons/dart_file.svg"); // 16x16
+  public static final Icon Dart_remote = load("/icons/dart_remote.svg"); // 16x16
+  public static final Icon Dart_test = load("/icons/dart_test.svg"); // 16x16
+  public static final Icon Dart_warning = load("/icons/dart_warning.png"); // 16x16
+  public static final Icon Feedback = load("/icons/feedback.png"); // 16x16
+  public static final Icon Observatory = load("/icons/observatory.png"); // 16x16
+  public static final Icon PubServeToolWindow = load("/icons/pubServeToolWindow.svg"); // 13x13
+  public static final Icon Restart_server = load("/icons/restart_server.png"); // 16x16
+  public static final Icon TestNode = load("/icons/testNode.png"); // 16x16
+  public static final Icon Warning_point = load("/icons/warning_point.png"); // 6x6
+}
