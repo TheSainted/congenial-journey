886c2988eb5564ca38a8968e60e1f9cb019e9a31
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:23:43 -0700

    Initialize 163.10154

---
+++ Dart/src/com/jetbrains/lang/dart/fixes/DartQuickFixSet.java
@@ -0,0 +1,77 @@
+package com.jetbrains.lang.dart.fixes;
+
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiManager;
+import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
+import com.jetbrains.lang.dart.ide.annotator.DartProblemGroup;
+import org.dartlang.analysis.server.protocol.AnalysisErrorFixes;
+import org.dartlang.analysis.server.protocol.SourceChange;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DartQuickFixSet {
+  private static final int MAX_QUICK_FIXES = 5;
+
+  @NotNull private final PsiManager myPsiManager;
+  @NotNull private final VirtualFile myFile;
+  private final int myOffset;
+  @Nullable private final String myErrorCode;
+  @NotNull private final String myErrorSeverity;
+
+  @NotNull private final List<DartQuickFix> myQuickFixes = new ArrayList<>(MAX_QUICK_FIXES);
+  private long myPsiModCount;
+
+
+  public DartQuickFixSet(@NotNull final PsiManager psiManager,
+                         @NotNull final VirtualFile file,
+                         final int offset,
+                         @Nullable final String errorCode,
+                         @NotNull final String errorSeverity) {
+    myPsiManager = psiManager;
+    myFile = file;
+    myOffset = offset;
+    myErrorCode = errorCode;
+    myErrorSeverity = errorSeverity;
+
+    for (int i = 0; i < MAX_QUICK_FIXES; i++) {
+      myQuickFixes.add(new DartQuickFix(this, i));
+    }
+  }
+
+  @NotNull
+  public List<DartQuickFix> getQuickFixes() {
+    return myQuickFixes;
+  }
+
+  void ensureInitialized() {
+    final long modCount = myPsiManager.getModificationTracker().getModificationCount();
+    if (myPsiModCount == modCount) return;
+
+    myPsiModCount = modCount;
+
+    for (DartQuickFix fix : myQuickFixes) {
+      fix.setSourceChange(null);
+    }
+
+    final List<AnalysisErrorFixes> fixes = DartAnalysisServerService.getInstance().edit_getFixes(myFile, myOffset);
+    if (fixes == null || fixes.isEmpty()) {
+      if (myErrorCode != null) {
+        myQuickFixes.get(0).setSuppressActionDelegate(new DartProblemGroup.DartSuppressAction(myErrorCode, myErrorSeverity, false));
+        myQuickFixes.get(1).setSuppressActionDelegate(new DartProblemGroup.DartSuppressAction(myErrorCode, myErrorSeverity, true));
+      }
+    }
+    else {
+      int index = 0;
+      for (AnalysisErrorFixes fix : fixes) {
+        for (SourceChange sourceChange : fix.getFixes()) {
+          myQuickFixes.get(index).setSourceChange(sourceChange);
+          index++;
+          if (index == MAX_QUICK_FIXES) return;
+        }
+      }
+    }
+  }
+}
