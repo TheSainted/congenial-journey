0b439391f3002f971f3858c82098369018c4387e
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:39:51 +0000

    Initialize ostrizhe/no_mapping_breakpoint

---
+++ Dart/src/com/jetbrains/lang/dart/DartPluginCapabilities.java
@@ -0,0 +1,22 @@
+package com.jetbrains.lang.dart;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A queryable class about the capabilities of the Dart plugin.
+ */
+@SuppressWarnings("unused")
+public class DartPluginCapabilities {
+  private static final Set<String> capabilities = new HashSet<>();
+
+  static {
+    capabilities.add("supports.pausePostRequest");
+  }
+
+  public static boolean isSupported(@NotNull String featureKey) {
+    return capabilities.contains(featureKey);
+  }
+}
