34477f04cd5c35eb40b036f1c017053e05e36490
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:41:07 +0000

    Initialize kk/non-modal-stuff

---
+++ Dart/testSrc/com/jetbrains/lang/dart/DartCodeInsightFixtureTestCase.java
@@ -0,0 +1,22 @@
+package com.jetbrains.lang.dart;
+
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import com.jetbrains.lang.dart.util.DartTestUtils;
+import org.jetbrains.annotations.NotNull;
+
+abstract public class DartCodeInsightFixtureTestCase extends LightPlatformCodeInsightFixtureTestCase {
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    DartTestUtils.configureDartSdk(myModule, myFixture.getProjectDisposable(), false);
+  }
+
+  @Override
+  protected String getTestDataPath() {
+    return DartTestUtils.BASE_TEST_DATA_PATH + getBasePath();
+  }
+
+  public void addStandardPackage(@NotNull final String packageName) {
+    myFixture.copyDirectoryToProject("../packages/" + packageName, "packages/" + packageName);
+  }
+}
