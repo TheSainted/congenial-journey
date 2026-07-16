65dd99c257695fdbaa3b8111956cca5e60105f56
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:50:02 -0700

    Initialize rider/altGr

---
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/VueJSCompletionContributor.kt
@@ -0,0 +1,25 @@
+package org.jetbrains.vuejs.codeInsight
+
+import com.intellij.codeInsight.completion.CompletionContributor
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.lang.javascript.psi.JSObjectLiteralExpression
+
+/**
+ * @author Irina.Chernushina on 7/31/2017.
+ */
+class VueJSCompletionContributor : CompletionContributor() {
+  override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
+    if (fillVueCompletionVariants(parameters, result)) return
+    super.fillCompletionVariants(parameters, result)
+  }
+
+  private fun fillVueCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) : Boolean {
+    val scriptWithExport = findScriptWithExport(parameters.position.originalElement) ?: return false
+    val defaultExport = scriptWithExport.second
+    val obj = defaultExport.stubSafeElement as? JSObjectLiteralExpression ?: return false
+    getComponentInnerDetailsFromObjectLiteral(obj).forEach { result.addElement(LookupElementBuilder.create(it.name)) }
+    return true
+  }
+}
