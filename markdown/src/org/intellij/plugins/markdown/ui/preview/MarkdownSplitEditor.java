d9708add301889bc10e41d1c1f504e09ca975dae
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:06:23 +0000

    Initialize ostrizhe/move_extensions

---
+++ markdown/src/org/intellij/plugins/markdown/ui/preview/MarkdownSplitEditor.java
@@ -0,0 +1,83 @@
+package org.intellij.plugins.markdown.ui.preview;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.event.CaretEvent;
+import com.intellij.openapi.editor.event.CaretListener;
+import com.intellij.openapi.fileEditor.TextEditor;
+import com.intellij.pom.Navigatable;
+import org.intellij.plugins.markdown.settings.MarkdownApplicationSettings;
+import org.intellij.plugins.markdown.ui.split.SplitFileEditor;
+import org.jetbrains.annotations.NotNull;
+
+public class MarkdownSplitEditor extends SplitFileEditor<TextEditor, MarkdownPreviewFileEditor> implements TextEditor {
+  private boolean myAutoScrollPreview = MarkdownApplicationSettings.getInstance().getMarkdownPreviewSettings().isAutoScrollPreview();
+
+  public MarkdownSplitEditor(@NotNull TextEditor mainEditor, @NotNull MarkdownPreviewFileEditor secondEditor) {
+    super(mainEditor, secondEditor);
+
+    MarkdownApplicationSettings.SettingsChangedListener settingsChangedListener =
+      new MarkdownApplicationSettings.SettingsChangedListener() {
+        @Override
+        public void beforeSettingsChanged(@NotNull MarkdownApplicationSettings newSettings) {
+          boolean oldAutoScrollPreview = MarkdownApplicationSettings.getInstance().getMarkdownPreviewSettings().isAutoScrollPreview();
+
+          ApplicationManager.getApplication().invokeLater(() -> {
+            if (oldAutoScrollPreview == myAutoScrollPreview) {
+              setAutoScrollPreview(newSettings.getMarkdownPreviewSettings().isAutoScrollPreview());
+            }
+          });
+        }
+      };
+
+    ApplicationManager.getApplication().getMessageBus().connect(this)
+      .subscribe(MarkdownApplicationSettings.SettingsChangedListener.TOPIC, settingsChangedListener);
+
+    mainEditor.getEditor().getCaretModel().addCaretListener(new MyCaretListener());
+  }
+
+  @NotNull
+  @Override
+  public String getName() {
+    return "Markdown split editor";
+  }
+
+  @NotNull
+  @Override
+  public Editor getEditor() {
+    return getMainEditor().getEditor();
+  }
+
+  @Override
+  public boolean canNavigateTo(@NotNull Navigatable navigatable) {
+    return getMainEditor().canNavigateTo(navigatable);
+  }
+
+  @Override
+  public void navigateTo(@NotNull Navigatable navigatable) {
+    getMainEditor().navigateTo(navigatable);
+  }
+
+  public boolean isAutoScrollPreview() {
+    return myAutoScrollPreview;
+  }
+
+  public void setAutoScrollPreview(boolean autoScrollPreview) {
+    myAutoScrollPreview = autoScrollPreview;
+  }
+
+  private class MyCaretListener implements CaretListener {
+    @Override
+    public void caretPositionChanged(CaretEvent e) {
+      if (!isAutoScrollPreview()) return;
+
+      final Editor editor = e.getEditor();
+      if (editor.getCaretModel().getCaretCount() != 1) {
+        return;
+      }
+
+      final int offset = editor.logicalPositionToOffset(e.getNewPosition());
+      getSecondEditor().scrollToSrcOffset(offset);
+    }
+  }
+}
