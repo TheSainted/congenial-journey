ee514bf9dc1e5e2be636c9efcb10b210c6f46098
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 19:53:45 -0700

    Initialize Idea14.0.4

---
+++ Dart/gen/com/jetbrains/lang/dart/psi/DartPartStatement.java
@@ -0,0 +1,19 @@
+// This is a generated file. Not intended for manual editing.
+package com.jetbrains.lang.dart.psi;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface DartPartStatement extends DartPsiCompositeElement {
+
+  @NotNull
+  List<DartMetadata> getMetadataList();
+
+  @NotNull
+  DartPathOrLibraryReference getPathOrLibraryReference();
+
+  @NotNull
+  String getPath();
+
+}
---
+++ Dart/gen/com/jetbrains/lang/dart/psi/impl/DartExportStatementImpl.java
@@ -0,0 +1,54 @@
+// This is a generated file. Not intended for manual editing.
+package com.jetbrains.lang.dart.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.jetbrains.lang.dart.DartTokenTypes.*;
+import com.jetbrains.lang.dart.psi.*;
+import com.jetbrains.lang.dart.util.DartPsiImplUtil;
+
+public class DartExportStatementImpl extends DartPsiCompositeElementImpl implements DartExportStatement {
+
+  public DartExportStatementImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof DartVisitor) ((DartVisitor)visitor).visitExportStatement(this);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public List<DartHideCombinator> getHideCombinatorList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, DartHideCombinator.class);
+  }
+
+  @Override
+  @NotNull
+  public List<DartMetadata> getMetadataList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, DartMetadata.class);
+  }
+
+  @Override
+  @NotNull
+  public List<DartShowCombinator> getShowCombinatorList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, DartShowCombinator.class);
+  }
+
+  @NotNull
+  public String getUri() {
+    return DartPsiImplUtil.getUri(this);
+  }
+
+  @Override
+  @NotNull
+  public DartPathOrLibraryReference getLibraryExpression() {
+    return findNotNullChildByClass(DartPathOrLibraryReference.class);
+  }
+
+}
