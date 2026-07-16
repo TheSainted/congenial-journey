6c528c1ab1ab8dc8f7a29bc5c7443a99e64a100c
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:47:06 +0000

    Initialize fus/separate-unknown-and-platform

---
+++ AngularJS/test/org/angular2/lang/expr/lexer/data/string.js
@@ -0,0 +1,19 @@
+'foo'
+'fo\u0042'
+'\n\r'
+"bar"
+'fo\uzzzz'
+'fo\u1'
+'fo\u12'
+'fo\u123'
+'fo\u'
+&apos;fo"o'
+&quot;fo'o"
+'fo&quot;dd'
+'fo\&apos;dd'
+'fo&aposdd'
+'fo&apos; + 12
+"fo&apos;dd"
+"fo\&quot;dd"
+'fo&quotdd'
+"fo&quot; + 12
