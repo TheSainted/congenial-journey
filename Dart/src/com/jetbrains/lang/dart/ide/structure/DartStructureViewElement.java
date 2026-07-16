955594c51f1048796accd7291d9557f4865975e7
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:46:30 -0700

    Initialize akoshevoy/unifySshCommunication

---
+++ Dart/src/com/jetbrains/lang/dart/ide/structure/DartStructureViewElement.java
@@ -0,0 +1,148 @@
+package com.jetbrains.lang.dart.ide.structure;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.structureView.StructureViewTreeElement;
+import com.intellij.navigation.ItemPresentation;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.containers.ContainerUtil;
+import com.jetbrains.lang.dart.DartComponentType;
+import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
+import com.jetbrains.lang.dart.util.DartPresentableUtil;
+import icons.DartIcons;
+import org.dartlang.analysis.server.protocol.Element;
+import org.dartlang.analysis.server.protocol.ElementKind;
+import org.dartlang.analysis.server.protocol.Outline;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class DartStructureViewElement implements StructureViewTreeElement, ItemPresentation {
+
+  @NotNull private final PsiFile myPsiFile;
+  @NotNull private final Outline myOutline;
+
+  public DartStructureViewElement(@NotNull final PsiFile psiFile, @NotNull final Outline outline) {
+    myPsiFile = psiFile;
+    myOutline = outline;
+  }
+
+  @Override
+  public Object getValue() {
+    return myOutline;
+  }
+
+  @NotNull
+  @Override
+  public ItemPresentation getPresentation() {
+    return this;
+  }
+
+  @NotNull
+  @Override
+  public StructureViewTreeElement[] getChildren() {
+    if (myOutline.getChildren().isEmpty()) return EMPTY_ARRAY;
+    return ContainerUtil.map2Array(myOutline.getChildren(), DartStructureViewElement.class,
+                                   outline -> new DartStructureViewElement(myPsiFile, outline));
+  }
+
+  @Override
+  public void navigate(boolean requestFocus) {
+    final DartAnalysisServerService service = DartAnalysisServerService.getInstance(myPsiFile.getProject());
+    final int offset = service.getConvertedOffset(myPsiFile.getVirtualFile(), myOutline.getElement().getLocation().getOffset());
+    new OpenFileDescriptor(myPsiFile.getProject(), myPsiFile.getVirtualFile(), offset).navigate(requestFocus);
+  }
+
+  @Override
+  public boolean canNavigate() {
+    return true;
+  }
+
+  @Override
+  public boolean canNavigateToSource() {
+    return true;
+  }
+
+  @Nullable
+  @Override
+  public String getPresentableText() {
+    final Element element = myOutline.getElement();
+    final StringBuilder b = new StringBuilder(element.getName());
+    if (!StringUtil.isEmpty(element.getTypeParameters())) {
+      b.append(element.getTypeParameters());
+    }
+    if (!StringUtil.isEmpty(element.getParameters())) {
+      b.append(element.getParameters());
+    }
+    if (!StringUtil.isEmpty(element.getReturnType())) {
+      b.append(" ").append(DartPresentableUtil.RIGHT_ARROW).append(" ").append(element.getReturnType());
+    }
+    return b.toString();
+  }
+
+  @Nullable
+  @Override
+  public String getLocationString() {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public Icon getIcon(boolean unused) {
+    final Icon baseIcon;
+    final Element element = myOutline.getElement();
+    switch (element.getKind()) {
+      case ElementKind.CLASS:
+        baseIcon = element.isAbstract() ? AllIcons.Nodes.AbstractClass : AllIcons.Nodes.Class;
+        break;
+      case ElementKind.CLASS_TYPE_ALIAS:
+        baseIcon = DartComponentType.TYPEDEF.getIcon();
+        break;
+      case ElementKind.CONSTRUCTOR:
+        baseIcon = DartComponentType.CONSTRUCTOR.getIcon();
+        break;
+      case ElementKind.ENUM:
+        baseIcon = AllIcons.Nodes.Enum;
+        break;
+      case ElementKind.ENUM_CONSTANT:
+        baseIcon = AllIcons.Nodes.Field;
+        break;
+      case ElementKind.FIELD:
+        baseIcon = DartComponentType.FIELD.getIcon();
+        break;
+      case ElementKind.FUNCTION:
+        baseIcon = DartComponentType.FUNCTION.getIcon();
+        break;
+      case ElementKind.FUNCTION_TYPE_ALIAS:
+        baseIcon = DartComponentType.TYPEDEF.getIcon();
+        break;
+      case ElementKind.GETTER:
+        baseIcon = AllIcons.Nodes.PropertyRead;
+        break;
+      case ElementKind.LOCAL_VARIABLE:
+        baseIcon = DartComponentType.VARIABLE.getIcon();
+        break;
+      case ElementKind.METHOD:
+        baseIcon = DartComponentType.METHOD.getIcon();
+        break;
+      case ElementKind.SETTER:
+        baseIcon = AllIcons.Nodes.PropertyWrite;
+        break;
+      case ElementKind.TOP_LEVEL_VARIABLE:
+        baseIcon = DartComponentType.VARIABLE.getIcon();
+        break;
+      default:
+        final String name = myOutline.getElement().getName();
+        if (name.startsWith("test ") || name.startsWith("group ")) {
+          baseIcon = DartIcons.TestNode;
+        }
+        else {
+          baseIcon = null;
+        }
+    }
+
+    return baseIcon;
+  }
+}
---
+++ struts2/plugin/src-test/com/intellij/struts2/dom/struts/StrutsResultResolvingTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2013 The authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intellij.struts2.dom.struts;
+
+import com.intellij.javaee.web.WebRoot;
+import com.intellij.javaee.web.facet.WebFacet;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.vfs.VfsUtilCore;
+import com.intellij.struts2.Struts2ProjectDescriptorBuilder;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Tests for {@link com.intellij.struts2.dom.struts.action.StrutsPathReferenceConverter}.
+ *
+ * @author Yann C&eacute;bron
+ */
+public class StrutsResultResolvingTest extends StrutsLightHighlightingTestCase {
+
+  private final LightProjectDescriptor WEB = new Struts2ProjectDescriptorBuilder()
+    .withStrutsLibrary()
+    .withStrutsFacet()
+    .withWebModuleType(getTestDataPath());
+  
+  @Override
+  @NotNull
+  protected String getTestDataLocation() {
+    return "strutsXml/result";
+  }
+
+  @NotNull
+  @Override
+  protected LightProjectDescriptor getProjectDescriptor() {
+    return WEB;
+  }
+
+  @Override
+  protected void performSetUp() {
+    final WebFacet webFacet = ContainerUtil.getFirstItem(WebFacet.getInstances(myModule));
+    assert webFacet != null;
+    WebRoot jsp = webFacet.addWebRoot(VfsUtilCore.pathToUrl(getTestDataPath() + "/jsp"), "/");
+    WebRoot jsp2 = webFacet.addWebRoot(VfsUtilCore.pathToUrl(getTestDataPath() + "/jsp2"), "2ndWebRoot");
+    Disposer.register(myFixture.getProjectDisposable(), () -> {
+      webFacet.removeWebRoot(jsp);
+      webFacet.removeWebRoot(jsp2);
+    });
+  }
+
+  /**
+   * @see com.intellij.struts2.dom.struts.impl.path.DispatchPathResultContributor
+   */
+  public void testPathDispatcher() {
+    performHighlightingTest("struts-path-dispatcher.xml");
+  }
+
+  /**
+   * @throws Throwable On errors.
+   * @see com.intellij.struts2.dom.struts.impl.path.ActionPathResultContributor
+   */
+  public void testActionPath() {
+    performHighlightingTest("struts-actionpath.xml");
+  }
+
+  /**
+   * @throws Throwable On errors.
+   * @see com.intellij.struts2.reference.jsp.ActionLinkReferenceProvider
+   */
+  public void testActionPathFQ() {
+    performHighlightingTest("struts-actionpath-fq.xml");
+  }
+
+  /**
+   * {@link com.intellij.struts2.dom.struts.impl.path.ActionChainOrRedirectResultContributor}
+   *
+   * @throws Throwable On errors.
+   */
+  public void testActionChain() {
+    performHighlightingTest("struts-actionchain.xml");
+  }
+
+  /**
+   * @throws Throwable On errors.
+   * @see com.intellij.struts2.dom.struts.impl.path.ActionChainOrRedirectResultContributor
+   */
+  public void testActionRedirect() {
+    performHighlightingTest("struts-actionRedirect.xml");
+  }
+
+  public void testUnknownResultTypes() {
+    performHighlightingTest("struts-unknownResultType.xml");
+  }
+
+  public void testCompletionVariantsDispatcherActionPath() {
+    performCompletionVariantTest("struts-completionvariants.xml",
+                                 "/anotherActionPathTest/anotherActionPath1.action",
+                                 "2ndWebRoot",
+                                 "actionPath1.action",
+                                 "index.jsp",
+                                 "jsp2-index.jsp",
+                                 "struts-completionvariants.xml");
+  }
+
+  public void testCompletionVariantsChain() {
+    performCompletionVariantTest("struts-completionvariants-chain.xml",
+                                 "/anotherActionPathTest/anotherActionPath1",
+                                 "actionPath1");
+  }
+}
