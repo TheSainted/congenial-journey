e0bce3ae4f51c5db6e344ef0e2578c50b2b9f2fb
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:40:00 -0700

    Initialize sdombrovsky/ui-improvements

---
+++ osmorc/src_test/org/osmorc/SwingRunner.java
@@ -0,0 +1,16 @@
+package org.osmorc;
+
+import com.intellij.util.ui.UIUtil;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+public class SwingRunner extends BlockJUnit4ClassRunner {
+  public SwingRunner(Class<?> klass) throws org.junit.runners.model.InitializationError {
+    super(klass);
+  }
+
+  @Override
+  public void run(final RunNotifier arg0) {
+    UIUtil.invokeAndWaitIfNeeded((Runnable)() -> SwingRunner.super.run(arg0));
+  }
+}
