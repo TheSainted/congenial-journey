dbb03136a16ff9dd903d9b850787df868d538b8f
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:47:21 +0000

    Initialize liza/var_view_new_kernel

---
+++ cucumber-java/test/org/jetbrains/plugins/cucumber/java/inspections/CucumberJavaCreateStepTest.java
@@ -0,0 +1,23 @@
+package org.jetbrains.plugins.cucumber.java.inspections;
+
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.plugins.cucumber.java.CucumberJavaTestUtil;
+
+public class CucumberJavaCreateStepTest extends AbstractCucumberJavaCreateStepTest {
+  public void testCreateAllSteps() {
+    doTest(true);
+  }
+
+  public void testStepWithSlash() {
+    doTest(false);
+  }
+
+  public void testJava8Step() {
+    doTest(false);
+  }
+
+  @Override
+  protected LightProjectDescriptor getProjectDescriptor() {
+    return CucumberJavaTestUtil.createCucumberJava8ProjectDescriptor();
+  }
+}
