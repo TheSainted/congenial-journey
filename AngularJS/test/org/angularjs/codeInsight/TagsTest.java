908c66a0dcc03c4e3f85887aed2e30a211a05ab3
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:13:07 -0700

    Initialize nz/patchSeparator

---
+++ AngularJS/test/org/angularjs/codeInsight/TagsTest.java
@@ -0,0 +1,247 @@
+package org.angularjs.codeInsight;
+
+import com.intellij.codeInspection.htmlInspections.RequiredAttributesInspection;
+import com.intellij.lang.javascript.JSTestUtils;
+import com.intellij.lang.javascript.dialects.JSLanguageLevel;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import com.intellij.util.ThrowableRunnable;
+import com.intellij.xml.util.CheckValidXmlInScriptBodyInspection;
+import org.angularjs.AngularTestUtil;
+
+import java.util.List;
+
+/**
+ * @author Dennis.Ushakov
+ */
+public class TagsTest extends LightPlatformCodeInsightFixtureTestCase {
+  @Override
+  protected String getTestDataPath() {
+    return AngularTestUtil.getBaseTestDataPath(getClass()) + "tags";
+  }
+
+  @Override
+  protected boolean isWriteActionRequired() {
+    return getTestName(true).contains("Completion");
+  }
+
+  public void testStandardTagsCompletion() {
+    myFixture.testCompletion("standard.html", "standard.after.html", "angular.js");
+  }
+
+  public void testStandardAttributesCompletion() {
+    myFixture.testCompletion("standardAttributes.html", "standardAttributes.after.html", "angular.js");
+  }
+
+  public void testStandardTagsResolve() {
+    myFixture.configureByFiles("standard.after.html", "angular.js");
+    int offsetBySignature = AngularTestUtil.findOffsetBySignature("ng-fo<caret>rm", myFixture.getFile());
+    PsiReference ref = myFixture.getFile().findReferenceAt(offsetBySignature);
+    assertNotNull(ref);
+    PsiElement resolve = ref.resolve();
+    assertNotNull(resolve);
+    assertEquals("angular.js", resolve.getContainingFile().getName());
+  }
+
+  public void testCustomTagsCompletion() {
+    myFixture.testCompletion("custom.html", "custom.after.html", "angular.js", "custom.js");
+  }
+
+  public void testCustomTagsCompletion13() {
+    myFixture.testCompletion("custom13.html", "custom13.after.html", "angular13.js", "custom.js");
+  }
+
+  public void testCustomTagsCompletion15() {
+    myFixture.testCompletion("custom13.html", "custom13.after.html", "angular13.js", "custom15.js");
+  }
+
+  public void testCustomTagsCompletion20TypeScript() throws Exception {
+    JSTestUtils.testWithinLanguageLevel(JSLanguageLevel.ES6, myFixture.getProject(),
+                                        (ThrowableRunnable<Exception>)() -> myFixture.testCompletion("custom.html", "custom.after.html", "angular2.js", "custom.ts"));
+  }
+
+  public void testCustomTagsCompletion20JavaScript() throws Exception {
+    JSTestUtils.testWithinLanguageLevel(JSLanguageLevel.ES6, myFixture.getProject(),
+                                        (ThrowableRunnable<Exception>)() -> myFixture.testCompletion("custom.html", "custom.after.html", "angular2.js", "custom2.js"));
+  }
+
+  public void testCustomTagsViaFunctionCompletion() {
+    myFixture.testCompletion("customViaFunction.html", "customViaFunction.after.html", "angular.js", "custom.js");
+  }
+
+  public void testCustomTagsArrayViaFunctionCompletion() {
+    myFixture.testCompletion("customArrayViaFunction.html", "customArrayViaFunction.after.html", "angular.js", "custom.js");
+  }
+
+  public void testCustomTagsViaFunctionForwardCompletion() {
+    myFixture.testCompletion("customViaFunctionForward.html", "customViaFunctionForward.after.html", "angular.js", "custom.js");
+  }
+
+  public void testCustomTagsArrayCompletion() {
+    myFixture.testCompletion("customArray.html", "customArray.after.html", "angular.js", "custom.js");
+  }
+
+  public void testCustomAttributesCompletion() {
+    myFixture.testCompletion("customAttributes.html", "customAttributes.after.html", "angular.js", "custom.js");
+  }
+
+  public void testCommonAttributesCompletion() {
+    myFixture.testCompletion("commonAttributes.html", "commonAttributes.after.html", "angular.js", "custom.js");
+  }
+
+  public void testCustomTagsResolve() {
+    myFixture.configureByFiles("custom.after.html", "angular.js", "custom.js");
+    int offsetBySignature = AngularTestUtil.findOffsetBySignature("my-cus<caret>tomer", myFixture.getFile());
+    PsiReference ref = myFixture.getFile().findReferenceAt(offsetBySignature);
+    assertNotNull(ref);
+    PsiElement resolve = ref.resolve();
+    assertNotNull(resolve);
+    assertEquals("custom.js", resolve.getContainingFile().getName());
+    assertEquals("'myCustomer'", AngularTestUtil.getDirectiveDefinitionText(resolve));
+  }
+
+  public void testCustomTagsResolve15() {
+    myFixture.configureByFiles("custom13.after.html", "angular.js", "custom15.js");
+    int offsetBySignature = AngularTestUtil.findOffsetBySignature("new-cus<caret>tomer", myFixture.getFile());
+    PsiReference ref = myFixture.getFile().findReferenceAt(offsetBySignature);
+    assertNotNull(ref);
+    PsiElement resolve = ref.resolve();
+    assertNotNull(resolve);
+    assertEquals("custom15.js", resolve.getContainingFile().getName());
+    assertEquals("'newCustomer'", AngularTestUtil.getDirectiveDefinitionText(resolve));
+  }
+
+  public void testCustomTagsResolve20TypeScriptComponent() throws Exception {
+    JSTestUtils.testWithinLanguageLevel(JSLanguageLevel.ES6, myFixture.getProject(), (ThrowableRunnable<Exception>)() -> {
+      myFixture.configureByFiles("custom.after.html", "angular2.js", "custom.ts");
+      int offsetBySignature = AngularTestUtil.findOffsetBySignature("my-cus<caret>tomer", myFixture.getFile());
+      PsiReference ref = myFixture.getFile().findReferenceAt(offsetBySignature);
+      assertNotNull(ref);
+      PsiElement resolve = ref.resolve();
+      assertNotNull(resolve);
+      assertEquals("custom.ts", resolve.getContainingFile().getName());
+      assertEquals("Component({\n" +
+                   "    selector: 'my-customer',\n" +
+                   "    properties: {\n" +
+                   "        'id':'dependency'\n" +
+                   "    }\n" +
+                   "})", AngularTestUtil.getDirectiveDefinitionText(resolve));
+    });
+  }
+
+  public void testCustomTagsResolve20TypeScriptDirective() throws Exception {
+    JSTestUtils.testWithinLanguageLevel(JSLanguageLevel.ES6, myFixture.getProject(), (ThrowableRunnable<Exception>)() -> {
+      myFixture.configureByFiles("custom.after.html", "angular2.js", "custom_directive.ts");
+      int offsetBySignature = AngularTestUtil.findOffsetBySignature("my-cus<caret>tomer", myFixture.getFile());
+      PsiReference ref = myFixture.getFile().findReferenceAt(offsetBySignature);
+      assertNotNull(ref);
+      PsiElement resolve = ref.resolve();
+      assertNotNull(resolve);
+      assertEquals("custom_directive.ts", resolve.getContainingFile().getName());
+      assertEquals("Directive({\n" +
+                   "    selector: 'my-customer',\n" +
+                   "    properties: {\n" +
+                   "        'id':'dependency'\n" +
+                   "    }\n" +
+                   "})", AngularTestUtil.getDirectiveDefinitionText(resolve));
+    });
+  }
+
+  public void testCustomTagsResolve20JavaScript() throws Exception {
+    JSTestUtils.testWithinLanguageLevel(JSLanguageLevel.ES6, myFixture.getProject(), (ThrowableRunnable<Exception>)() -> {
+      myFixture.configureByFiles("custom.after.html", "angular2.js", "custom2.js");
+      int offsetBySignature = AngularTestUtil.findOffsetBySignature("my-cus<caret>tomer", myFixture.getFile());
+      PsiReference ref = myFixture.getFile().findReferenceAt(offsetBySignature);
+      assertNotNull(ref);
+      PsiElement resolve = ref.resolve();
+      assertNotNull(resolve);
+      assertEquals("custom2.js", resolve.getContainingFile().getName());
+      assertEquals("new angular.ComponentAnnotation({\n" +
+                   "    selector: 'my-customer'\n" +
+                   "  })", AngularTestUtil.getDirectiveDefinitionText(resolve));
+    });
+  }
+
+  public void testCustomTagsViaFunctionResolve() {
+    myFixture.configureByFiles("customViaFunction.after.html", "angular.js", "custom.js");
+    int offsetBySignature = AngularTestUtil.findOffsetBySignature("function-cus<caret>tomer", myFixture.getFile());
+    PsiReference ref = myFixture.getFile().findReferenceAt(offsetBySignature);
+    assertNotNull(ref);
+    PsiElement resolve = ref.resolve();
+    assertNotNull(resolve);
+    assertEquals("custom.js", resolve.getContainingFile().getName());
+    assertEquals("'functionCustomer'", AngularTestUtil.getDirectiveDefinitionText(resolve));
+  }
+
+  public void testCustomTagsViaFunctionForwardResolve() {
+    myFixture.configureByFiles("customViaFunctionForward.after.html", "angular.js", "custom.js");
+    int offsetBySignature = AngularTestUtil.findOffsetBySignature("great-cus<caret>tomer", myFixture.getFile());
+    PsiReference ref = myFixture.getFile().findReferenceAt(offsetBySignature);
+    assertNotNull(ref);
+    PsiElement resolve = ref.resolve();
+    assertNotNull(resolve);
+    assertEquals("custom.js", resolve.getContainingFile().getName());
+    assertEquals("'greatCustomer'", AngularTestUtil.getDirectiveDefinitionText(resolve));
+  }
+
+
+  public void testCustomTagsArrayViaFunctionResolve() {
+    myFixture.configureByFiles("customArrayViaFunction.after.html", "angular.js", "custom.js");
+    int offsetBySignature = AngularTestUtil.findOffsetBySignature("array-cus<caret>tomer", myFixture.getFile());
+    PsiReference ref = myFixture.getFile().findReferenceAt(offsetBySignature);
+    assertNotNull(ref);
+    PsiElement resolve = ref.resolve();
+    assertNotNull(resolve);
+    assertEquals("custom.js", resolve.getContainingFile().getName());
+    assertEquals("'arrayCustomer'", AngularTestUtil.getDirectiveDefinitionText(resolve));
+  }
+
+  public void testCustomTagsArrayResolve() {
+    myFixture.configureByFiles("customArray.after.html", "angular.js", "custom.js");
+    int offsetBySignature = AngularTestUtil.findOffsetBySignature("her-cus<caret>tomer", myFixture.getFile());
+    PsiReference ref = myFixture.getFile().findReferenceAt(offsetBySignature);
+    assertNotNull(ref);
+    PsiElement resolve = ref.resolve();
+    assertNotNull(resolve);
+    assertEquals("custom.js", resolve.getContainingFile().getName());
+    assertEquals("'herCustomer'", AngularTestUtil.getDirectiveDefinitionText(resolve));
+  }
+
+  public void testOverride() {
+    myFixture.enableInspections(CheckValidXmlInScriptBodyInspection.class);
+    myFixture.configureByFiles("override.html", "angular.js");
+    myFixture.checkHighlighting();
+  }
+
+  public void testProperNamespace() {
+    myFixture.enableInspections(RequiredAttributesInspection.class);
+    myFixture.configureByFiles("namespace.html", "angular.js", "custom.js");
+    myFixture.checkHighlighting();
+  }
+
+  public void testCustomTagsCompletionCss() {
+    myFixture.testCompletion("customCss.html", "customCss.after.html", "angular.js", "custom.js");
+  }
+
+  public void testCustomTagsResolveCss() {
+    myFixture.configureByFiles("customCss.after.html", "angular.js", "custom.js");
+    int offsetBySignature = AngularTestUtil.findOffsetBySignature("my-cus<caret>tomer", myFixture.getFile());
+    PsiReference ref = myFixture.getFile().findReferenceAt(offsetBySignature);
+    assertNotNull(ref);
+    PsiElement resolve = ref.resolve();
+    assertNotNull(resolve);
+    assertEquals("custom.js", resolve.getContainingFile().getName());
+    assertEquals("'myCustomer'", AngularTestUtil.getDirectiveDefinitionText(resolve));
+  }
+
+  public void testNoCompletionInXml() {
+    final List<String> variants = myFixture.getCompletionVariants("standard.xml", "angular.js");
+    assertDoesntContain(variants, "ng-form", "form", "script");
+  }
+
+  public void testUnclosed() {
+    myFixture.configureByFiles("unclosed.html", "angular.js", "custom.js");
+    myFixture.checkHighlighting();
+  }
+}
---
+++ Dart/src/com/jetbrains/lang/dart/ide/actions/DartPubBuildDialog.java
@@ -0,0 +1,176 @@
+package com.jetbrains.lang.dart.ide.actions;
+
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.TextComponentAccessor;
+import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.ui.ValidationInfo;
+import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.IdeFocusManager;
+import com.intellij.ui.components.JBRadioButton;
+import com.jetbrains.lang.dart.DartBundle;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DartPubBuildDialog extends DialogWrapper {
+
+  private static final String DART_PUB_BUILD_MODE_KEY = "DART_PUB_BUILD_MODE";
+  private static final String DART_PUB_CUSTOM_BUILD_MODE_KEY = "DART_PUB_CUSTOM_BUILD_MODE";
+
+  private static final String RELEASE_MODE = "release";
+  private static final String DEBUG_MODE = "debug";
+  private static final String OTHER_MODE = "other";
+  private static final String DEFAULT_MODE = RELEASE_MODE;
+
+  private static final String DART_PUB_BUILD_OUTPUT_KEY = "DART_PUB_BUILD_OUTPUT_KEY";
+  private static final String DEFAULT_OUTPUT_FOLDER = "build";
+
+  private JPanel myMainPanel;
+  private JBRadioButton myReleaseRadioButton;
+  private JBRadioButton myDebugRadioButton;
+  private JBRadioButton myOtherRadioButton;
+  private JTextField myOtherModeTextField;
+  private TextFieldWithBrowseButton myOutputFolderField;
+
+  private final @NotNull Project myProject;
+
+  public DartPubBuildDialog(@NotNull final Project project, @NotNull final VirtualFile packageDir) {
+    super(project);
+    myProject = project;
+
+    setTitle(DartBundle.message("dart.pub.build.title"));
+
+    ActionListener listener = new ActionListener() {
+      public void actionPerformed(final ActionEvent e) {
+        updateControls();
+      }
+    };
+    myReleaseRadioButton.addActionListener(listener);
+    myDebugRadioButton.addActionListener(listener);
+    myOtherRadioButton.addActionListener(listener);
+    myOtherRadioButton.addActionListener(new ActionListener() {
+      public void actionPerformed(final ActionEvent e) {
+        if (myOtherRadioButton.isSelected()) {
+          IdeFocusManager.getInstance(myProject).requestFocus(myOtherModeTextField, true);
+        }
+      }
+    });
+
+    final String packagePathSlash = FileUtil.toSystemDependentName(packageDir.getPath() + "/");
+    myOutputFolderField.addBrowseFolderListener("Output Folder", null, myProject,
+                                                FileChooserDescriptorFactory.createSingleFolderDescriptor(),
+                                                new TextComponentAccessor<JTextField>() {
+                                                  @Override
+                                                  public String getText(JTextField component) {
+                                                    final String path = component.getText();
+                                                    if (SystemInfo.isWindows && FileUtil.isWindowsAbsolutePath(path) ||
+                                                        !SystemInfo.isWindows && FileUtil.isUnixAbsolutePath(path)) {
+                                                      return path;
+                                                    }
+
+                                                    return packagePathSlash + path;
+                                                  }
+
+                                                  @Override
+                                                  public void setText(JTextField component, @NotNull String text) {
+                                                    if (text.startsWith(packagePathSlash) && !text.equals(packagePathSlash)) {
+                                                      component.setText(text.substring(packagePathSlash.length()));
+                                                    }
+                                                    else {
+                                                      component.setText(text);
+                                                    }
+                                                  }
+                                                });
+
+    reset();
+    init();
+  }
+
+  private void reset() {
+    final PropertiesComponent propertiesComponent = PropertiesComponent.getInstance(myProject);
+
+    final String mode = propertiesComponent.getValue(DART_PUB_BUILD_MODE_KEY, DEFAULT_MODE);
+    if (mode.equals(RELEASE_MODE)) {
+      myReleaseRadioButton.setSelected(true);
+    }
+    else if (mode.equals(DEBUG_MODE)) {
+      myDebugRadioButton.setSelected(true);
+    }
+    else {
+      myOtherRadioButton.setSelected(true);
+    }
+
+    myOtherModeTextField.setText(propertiesComponent.getValue(DART_PUB_CUSTOM_BUILD_MODE_KEY, ""));
+
+    myOutputFolderField.setText(propertiesComponent.getValue(DART_PUB_BUILD_OUTPUT_KEY, DEFAULT_OUTPUT_FOLDER));
+
+    updateControls();
+  }
+
+  private void updateControls() {
+    myOtherModeTextField.setEnabled(myOtherRadioButton.isSelected());
+  }
+
+  @Nullable
+  protected JComponent createCenterPanel() {
+    return myMainPanel;
+  }
+
+  @Nullable
+  public JComponent getPreferredFocusedComponent() {
+    if (myOtherRadioButton.isSelected()) return myOtherModeTextField;
+    return null;
+  }
+
+  @Nullable
+  protected ValidationInfo doValidate() {
+    if (myOtherRadioButton.isSelected() && StringUtil.isEmptyOrSpaces(myOtherModeTextField.getText())) {
+      return new ValidationInfo(DartBundle.message("pub.build.mode.not.specified"));
+    }
+    return null;
+  }
+
+  protected void doOKAction() {
+    saveDialogState();
+    super.doOKAction();
+  }
+
+  private void saveDialogState() {
+    final PropertiesComponent propertiesComponent = PropertiesComponent.getInstance(myProject);
+
+    final String mode = myReleaseRadioButton.isSelected() ? RELEASE_MODE
+                                                          : myDebugRadioButton.isSelected() ? DEBUG_MODE
+                                                                                            : OTHER_MODE;
+    propertiesComponent.setValue(DART_PUB_BUILD_MODE_KEY, mode, DEFAULT_MODE);
+
+    if (myOtherRadioButton.isSelected()) {
+      propertiesComponent.setValue(DART_PUB_CUSTOM_BUILD_MODE_KEY, myOtherModeTextField.getText().trim());
+    }
+
+    final String outputPath = StringUtil.nullize(myOutputFolderField.getText().trim());
+    propertiesComponent.setValue(DART_PUB_BUILD_OUTPUT_KEY, outputPath, DEFAULT_OUTPUT_FOLDER);
+  }
+
+  @NotNull
+  public String getPubBuildMode() {
+    if (myReleaseRadioButton.isSelected()) return RELEASE_MODE;
+    if (myDebugRadioButton.isSelected()) return DEBUG_MODE;
+    return myOtherModeTextField.getText().trim();
+  }
+
+  @NotNull
+  public String getOutputFolder() {
+    String path = myOutputFolderField.getText().trim();
+    if (path.isEmpty()) path = DEFAULT_OUTPUT_FOLDER;
+    return path;
+  }
+}
