960126fdbaf2347a88fd26eae84e3faab6257bb2
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:19:40 -0700

    Initialize rider/contentModelChanges

---
+++ Dart/src/com/jetbrains/lang/dart/ide/completion/DartServerCompletionContributor.java
@@ -0,0 +1,317 @@
+package com.jetbrains.lang.dart.ide.completion;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.intellij.codeInsight.AutoPopupController;
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.completion.util.ParenthesesInsertHandler;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.highlighter.HtmlFileType;
+import com.intellij.injected.editor.VirtualFileWindow;
+import com.intellij.lang.html.HTMLLanguage;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.xml.XmlTokenType;
+import com.intellij.ui.LayeredIcon;
+import com.intellij.ui.RowIcon;
+import com.intellij.util.PlatformIcons;
+import com.intellij.util.ProcessingContext;
+import com.jetbrains.lang.dart.DartLanguage;
+import com.jetbrains.lang.dart.DartYamlFileTypeFactory;
+import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
+import com.jetbrains.lang.dart.psi.DartStringLiteralExpression;
+import com.jetbrains.lang.dart.psi.DartUriElement;
+import com.jetbrains.lang.dart.sdk.DartSdk;
+import com.jetbrains.lang.dart.util.DartResolveUtil;
+import com.jetbrains.lang.dart.util.PubspecYamlUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.dartlang.analysis.server.protocol.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.List;
+
+import static com.intellij.patterns.PlatformPatterns.psiElement;
+import static com.intellij.patterns.PlatformPatterns.psiFile;
+import static com.intellij.patterns.StandardPatterns.or;
+
+public class DartServerCompletionContributor extends CompletionContributor {
+  public DartServerCompletionContributor() {
+    extend(CompletionType.BASIC,
+           or(psiElement().withLanguage(DartLanguage.INSTANCE),
+              psiElement().inFile(psiFile().withLanguage(HTMLLanguage.INSTANCE)),
+              psiElement().inFile(psiFile().withName(DartYamlFileTypeFactory.DOT_ANALYSIS_OPTIONS))),
+           new CompletionProvider<CompletionParameters>() {
+             @Override
+             protected void addCompletions(@NotNull final CompletionParameters parameters,
+                                           @NotNull final ProcessingContext context,
+                                           @NotNull final CompletionResultSet originalResultSet) {
+               VirtualFile file = DartResolveUtil.getRealVirtualFile(parameters.getOriginalFile());
+               if (file instanceof VirtualFileWindow) {
+                 file = ((VirtualFileWindow)file).getDelegate();
+               }
+
+               if (file == null) return;
+
+               final Project project = parameters.getOriginalFile().getProject();
+
+               if (file.getFileType() == HtmlFileType.INSTANCE && PubspecYamlUtil.findPubspecYamlFile(project, file) == null) {
+                 return;
+               }
+
+               final DartSdk sdk = DartSdk.getDartSdk(project);
+               if (sdk == null || !DartAnalysisServerService.isDartSdkVersionSufficient(sdk)) return;
+
+               DartAnalysisServerService.getInstance().updateFilesContent();
+
+               final String completionId =
+                 DartAnalysisServerService.getInstance().completion_getSuggestions(file, parameters.getOffset());
+               if (completionId == null) return;
+
+               final String specialPrefix = getSpecialPrefix(parameters);
+               final CompletionResultSet resultSet = specialPrefix != null
+                                                     ? originalResultSet.withPrefixMatcher(specialPrefix)
+                                                     : originalResultSet;
+
+               DartAnalysisServerService.getInstance().addCompletions(completionId, suggestion -> {
+                 final LookupElement lookupElement = createLookupElement(project, suggestion);
+                 resultSet.addElement(lookupElement);
+               });
+             }
+           });
+  }
+
+  @Nullable
+  private static String getSpecialPrefix(@NotNull final CompletionParameters parameters) {
+    final PsiElement psiElement = parameters.getOriginalPosition();
+    if (psiElement == null) return null;
+
+    final PsiElement parent = psiElement.getParent();
+    if (parent instanceof DartStringLiteralExpression) {
+      final PsiElement parentParent = parent.getParent();
+      if (parentParent instanceof DartUriElement) {
+        final int uriStringOffset = ((DartUriElement)parentParent).getUriStringAndItsRange().second.getStartOffset();
+        if (parameters.getOffset() >= parentParent.getTextRange().getStartOffset() + uriStringOffset) {
+          return parentParent.getText().substring(uriStringOffset, parameters.getOffset() - parentParent.getTextRange().getStartOffset());
+        }
+      }
+      else {
+        return getIdentifierPrefix(parameters);
+      }
+    }
+
+    final IElementType elementType = psiElement.getNode().getElementType();
+    if (elementType == XmlTokenType.XML_DATA_CHARACTERS || elementType == XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN) {
+      return getIdentifierPrefix(parameters);
+    }
+
+    return null;
+  }
+
+  @Nullable
+  private static String getIdentifierPrefix(@NotNull final CompletionParameters parameters) {
+    final PsiElement element = parameters.getOriginalPosition();
+    if (element == null) return null;
+
+    final int offset = parameters.getOffset();
+    final TextRange range = element.getTextRange();
+    if (offset < range.getStartOffset() || offset > range.getEndOffset()) return null;
+
+    return getIdentifierInTheEndOfThisString(element.getText().substring(0, parameters.getOffset() - range.getStartOffset()));
+  }
+
+  /**
+   * If <code>text</code> ends with any valid identifier - this identifier is returned, otherwise empty string is returned.
+   * For example if text="foo.bar123" then "bar123" is returned; if text="foo." then empty string is returned.
+   */
+  @NotNull
+  @VisibleForTesting
+  public static String getIdentifierInTheEndOfThisString(@NotNull final String text) {
+    int prefixStartIndex = text.length();
+    for (int i = text.length() - 1; i >= 0; i--) {
+      char ch = text.charAt(i);
+      if (!Character.isJavaIdentifierPart(ch)) {
+        return text.substring(prefixStartIndex);
+      }
+
+      if (Character.isJavaIdentifierStart(ch)) {
+        prefixStartIndex = i;
+      }
+    }
+
+    return text.substring(prefixStartIndex);
+  }
+
+  @Override
+  public void beforeCompletion(@NotNull final CompletionInitializationContext context) {
+    final PsiElement psiElement = context.getFile().findElementAt(context.getStartOffset());
+    final PsiElement parent = psiElement != null ? psiElement.getParent() : null;
+    final PsiElement parentParent = parent instanceof DartStringLiteralExpression ? parent.getParent() : null;
+    if (parentParent instanceof DartUriElement) {
+      final Pair<String, TextRange> uriAndRange = ((DartUriElement)parentParent).getUriStringAndItsRange();
+      context.setReplacementOffset(parentParent.getTextRange().getStartOffset() + uriAndRange.second.getEndOffset());
+    }
+  }
+
+  private static Icon applyOverlay(Icon base, boolean condition, Icon overlay) {
+    if (condition) {
+      return new LayeredIcon(base, overlay);
+    }
+    return base;
+  }
+
+  private static Icon applyVisibility(Icon base, boolean isPrivate) {
+    RowIcon result = new RowIcon(2);
+    result.setIcon(base, 0);
+    Icon visibility = isPrivate ? PlatformIcons.PRIVATE_ICON : PlatformIcons.PUBLIC_ICON;
+    result.setIcon(visibility, 1);
+    return result;
+  }
+
+  private static LookupElement createLookupElement(@NotNull final Project project, @NotNull final CompletionSuggestion suggestion) {
+    final Element element = suggestion.getElement();
+    final Location location = element == null ? null : element.getLocation();
+    final DartLookupObject lookupObject = new DartLookupObject(project, location, suggestion.getRelevance());
+
+    final String lookupString = suggestion.getCompletion();
+    LookupElementBuilder lookup = LookupElementBuilder.create(lookupObject, lookupString);
+
+    // keywords are bold
+    if (suggestion.getKind().equals(CompletionSuggestionKind.KEYWORD)) {
+      lookup = lookup.bold();
+    }
+
+    final int dotIndex = lookupString.indexOf('.');
+    if (dotIndex > 0 && dotIndex < lookupString.length() - 1 &&
+        StringUtil.isJavaIdentifier(lookupString.substring(0, dotIndex)) &&
+        StringUtil.isJavaIdentifier(lookupString.substring(dotIndex + 1))) {
+      // 'path.Context' should match 'Conte' prefix
+      lookup = lookup.withLookupString(lookupString.substring(dotIndex + 1));
+    }
+
+    boolean shouldSetSelection = true;
+    if (element != null) {
+      // @deprecated
+      if (element.isDeprecated()) {
+        lookup = lookup.strikeout();
+      }
+      // append type parameters
+      final String typeParameters = element.getTypeParameters();
+      if (typeParameters != null) {
+        lookup = lookup.appendTailText(typeParameters, false);
+      }
+      // append parameters
+      final String parameters = element.getParameters();
+      if (parameters != null) {
+        lookup = lookup.appendTailText(parameters, false);
+      }
+      // append return type
+      final String returnType = element.getReturnType();
+      if (!StringUtils.isEmpty(returnType)) {
+        lookup = lookup.withTypeText(returnType, true);
+      }
+      // icon
+      Icon icon = getBaseImage(element);
+      if (icon != null) {
+        icon = applyVisibility(icon, element.isPrivate());
+        icon = applyOverlay(icon, element.isFinal(), AllIcons.Nodes.FinalMark);
+        icon = applyOverlay(icon, element.isConst(), AllIcons.Nodes.FinalMark);
+        lookup = lookup.withIcon(icon);
+      }
+      // Prepare for typing arguments, if any.
+      if (CompletionSuggestionKind.INVOCATION.equals(suggestion.getKind())) {
+        shouldSetSelection = false;
+        final List<String> parameterNames = suggestion.getParameterNames();
+        if (parameterNames != null) {
+          lookup = lookup.withInsertHandler(new InsertHandler<LookupElement>() {
+            @Override
+            public void handleInsert(InsertionContext context, LookupElement item) {
+              if (parameterNames.isEmpty()) {
+                ParenthesesInsertHandler.NO_PARAMETERS.handleInsert(context, item);
+              }
+              else {
+                ParenthesesInsertHandler.WITH_PARAMETERS.handleInsert(context, item);
+                // Show parameters popup.
+                final Editor editor = context.getEditor();
+                final PsiElement psiElement = lookupObject.getElement();
+                AutoPopupController.getInstance(project).autoPopupParameterInfo(editor, psiElement);
+              }
+            }
+          });
+        }
+      }
+    }
+
+    // Use selection offset / length.
+    if (shouldSetSelection) {
+      lookup = lookup.withInsertHandler(new InsertHandler<LookupElement>() {
+        @Override
+        public void handleInsert(InsertionContext context, LookupElement item) {
+          final Editor editor = context.getEditor();
+          final int startOffset = context.getStartOffset() + suggestion.getSelectionOffset();
+          final int endOffset = startOffset + suggestion.getSelectionLength();
+          editor.getCaretModel().moveToOffset(startOffset);
+          if (endOffset > startOffset) {
+            editor.getSelectionModel().setSelection(startOffset, endOffset);
+          }
+        }
+      });
+    }
+
+    return lookup;
+  }
+
+  private static Icon getBaseImage(Element element) {
+    final String elementKind = element.getKind();
+    if (elementKind.equals(ElementKind.CLASS) || elementKind.equals(ElementKind.CLASS_TYPE_ALIAS)) {
+      if (element.isAbstract()) {
+        return AllIcons.Nodes.AbstractClass;
+      }
+      return AllIcons.Nodes.Class;
+    }
+    else if (elementKind.equals(ElementKind.ENUM)) {
+      return AllIcons.Nodes.Enum;
+    }
+    else if (elementKind.equals(ElementKind.ENUM_CONSTANT) || elementKind.equals(ElementKind.FIELD)) {
+      return AllIcons.Nodes.Field;
+    }
+    else if (elementKind.equals(ElementKind.COMPILATION_UNIT)) {
+      return PlatformIcons.FILE_ICON;
+    }
+    else if (elementKind.equals(ElementKind.CONSTRUCTOR)) {
+      return AllIcons.Nodes.ClassInitializer;
+    }
+    else if (elementKind.equals(ElementKind.GETTER)) {
+      return element.isTopLevelOrStatic() ? AllIcons.Nodes.PropertyReadStatic : AllIcons.Nodes.PropertyRead;
+    }
+    else if (elementKind.equals(ElementKind.SETTER)) {
+      return element.isTopLevelOrStatic() ? AllIcons.Nodes.PropertyWriteStatic : AllIcons.Nodes.PropertyWrite;
+    }
+    else if (elementKind.equals(ElementKind.METHOD)) {
+      if (element.isAbstract()) {
+        return AllIcons.Nodes.AbstractMethod;
+      }
+      return AllIcons.Nodes.Method;
+    }
+    else if (elementKind.equals(ElementKind.FUNCTION)) {
+      return AllIcons.Nodes.Function;
+    }
+    else if (elementKind.equals(ElementKind.FUNCTION_TYPE_ALIAS)) {
+      return AllIcons.Nodes.Annotationtype;
+    }
+    else if (elementKind.equals(ElementKind.TOP_LEVEL_VARIABLE)) {
+      return AllIcons.Nodes.Variable;
+    }
+    else {
+      return null;
+    }
+  }
+}
---
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/test/DartTestRunningState.java
@@ -0,0 +1,204 @@
+package com.jetbrains.lang.dart.ide.runner.test;
+
+import com.intellij.execution.DefaultExecutionResult;
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.ExecutionResult;
+import com.intellij.execution.Executor;
+import com.intellij.execution.configurations.RuntimeConfigurationError;
+import com.intellij.execution.filters.UrlFilter;
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.runners.ProgramRunner;
+import com.intellij.execution.testframework.TestConsoleProperties;
+import com.intellij.execution.testframework.actions.AbstractRerunFailedTestsAction;
+import com.intellij.execution.testframework.autotest.ToggleAutoTestAction;
+import com.intellij.execution.testframework.sm.SMCustomMessagesParsing;
+import com.intellij.execution.testframework.sm.SMTestRunnerConnectionUtil;
+import com.intellij.execution.testframework.sm.runner.OutputToGeneralTestEventsConverter;
+import com.intellij.execution.testframework.sm.runner.SMTRunnerConsoleProperties;
+import com.intellij.execution.testframework.sm.runner.SMTestLocator;
+import com.intellij.execution.testframework.sm.runner.ui.SMTRunnerConsoleView;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.registry.Registry;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VfsUtilCore;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.jetbrains.lang.dart.ide.runner.DartConsoleFilter;
+import com.jetbrains.lang.dart.ide.runner.DartRelativePathsConsoleFilter;
+import com.jetbrains.lang.dart.ide.runner.base.DartRunConfiguration;
+import com.jetbrains.lang.dart.ide.runner.server.DartCommandLineRunningState;
+import com.jetbrains.lang.dart.ide.runner.util.DartTestLocationProvider;
+import com.jetbrains.lang.dart.sdk.DartSdk;
+import com.jetbrains.lang.dart.util.DartUrlResolver;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class DartTestRunningState extends DartCommandLineRunningState {
+  public static final String DART_FRAMEWORK_NAME = "DartTestRunner";
+  private static final String PUB_SNAPSHOT_PATH = "/bin/snapshots/pub.dart.snapshot";
+  private static final String RUN_COMMAND = "run";
+  private static final String TEST_PACKAGE_SPEC = "test:test";
+  private static final String EXPANDED_REPORTER_OPTION = "-r json";
+
+  public DartTestRunningState(final @NotNull ExecutionEnvironment environment) throws ExecutionException {
+    super(environment);
+  }
+
+  @Override
+  @NotNull
+  public ExecutionResult execute(final @NotNull Executor executor, final @NotNull ProgramRunner runner) throws ExecutionException {
+    final ProcessHandler processHandler = startProcess();
+    final ConsoleView consoleView = createConsole(getEnvironment());
+    consoleView.attachToProcess(processHandler);
+
+    final DefaultExecutionResult executionResult =
+      new DefaultExecutionResult(consoleView, processHandler, createActions(consoleView, processHandler, executor));
+
+    if (ActionManager.getInstance().getAction("RerunFailedTests") != null) {
+      DartConsoleProperties properties = (DartConsoleProperties)((SMTRunnerConsoleView)consoleView).getProperties();
+      AbstractRerunFailedTestsAction rerunFailedTestsAction = properties.createRerunFailedTestsAction(consoleView);
+      assert rerunFailedTestsAction != null;
+      rerunFailedTestsAction.setModelProvider(() -> ((SMTRunnerConsoleView)consoleView).getResultsViewer());
+      executionResult.setRestartActions(rerunFailedTestsAction, new ToggleAutoTestAction());
+    }
+    else {
+      executionResult.setRestartActions(new ToggleAutoTestAction());
+    }
+
+    return executionResult;
+  }
+
+  @Override
+  protected void addObservatoryActions(List<AnAction> actions, final ProcessHandler processHandler) {
+    // For now, don't add the Observatory button to the test view; it's currently a no-op.
+    // Until debugging works with package:test, the observatory URL we open here is 'http://127.0.0.1:-1'.
+  }
+
+  private static ConsoleView createConsole(@NotNull ExecutionEnvironment env) {
+    final Project project = env.getProject();
+    final DartRunConfiguration runConfiguration = (DartRunConfiguration)env.getRunProfile();
+    final DartTestRunnerParameters runnerParameters = (DartTestRunnerParameters)runConfiguration.getRunnerParameters();
+
+    final TestConsoleProperties testConsoleProperties = new DartConsoleProperties(runConfiguration, env);
+    final ConsoleView consoleView = SMTestRunnerConnectionUtil.createConsole(DART_FRAMEWORK_NAME, testConsoleProperties);
+
+    try {
+      final VirtualFile dartFile = runnerParameters.getDartFileOrDirectory();
+      consoleView.addMessageFilter(new DartConsoleFilter(project, dartFile));
+      consoleView.addMessageFilter(new DartRelativePathsConsoleFilter(project, runnerParameters.computeProcessWorkingDirectory(project)));
+      consoleView.addMessageFilter(new UrlFilter());
+    }
+    catch (RuntimeConfigurationError ignore) {/* can't happen because already checked */}
+
+    Disposer.register(project, consoleView);
+    return consoleView;
+  }
+
+  @NotNull
+  @Override
+  protected ProcessHandler startProcess() throws ExecutionException {
+    Project project = getEnvironment().getProject();
+
+    DartSdk sdk = DartSdk.getDartSdk(project);
+    if (sdk == null) throw new ExecutionException("Dart SDK cannot be found"); // can't happen, already checked
+
+    DartTestRunnerParameters params = getParameters();
+
+    StringBuilder builder = new StringBuilder();
+    builder.append(RUN_COMMAND);
+
+    final boolean projectWithoutPubspec = Registry.is("dart.projects.without.pubspec", false);
+    final String targetName = params.getTargetName();
+    final String testRunnerOptions = params.getTestRunnerOptions();
+
+    if (projectWithoutPubspec &&
+        params.getScope() == DartTestRunnerParameters.Scope.FOLDER &&
+        targetName != null &&
+        !targetName.isEmpty()) {
+      builder.append(" ").append(":").append(targetName).append(" ").append(EXPANDED_REPORTER_OPTION);
+      if (testRunnerOptions != null && !testRunnerOptions.isEmpty()) {
+        builder.append(" ").append(testRunnerOptions);
+      }
+    }
+    else {
+      builder.append(' ').append(TEST_PACKAGE_SPEC);
+      builder.append(' ').append(EXPANDED_REPORTER_OPTION);
+      if (testRunnerOptions != null && !testRunnerOptions.isEmpty()) {
+        builder.append(" ").append(testRunnerOptions);
+      }
+      builder.append(' ').append(params.getFilePath());
+
+      if (params.getScope() == DartTestRunnerParameters.Scope.GROUP_OR_TEST_BY_NAME) {
+        builder.append(" -N \"").append(StringUtil.notNullize(params.getTestName())).append("\"");
+      }
+
+      if (params.getScope() == DartTestRunnerParameters.Scope.MULTIPLE_NAMES) {
+        final String regex = StringUtil.notNullize(params.getTestName());
+        // may be empty only in case of Rerun Failed Tests when there are no failed ones yet
+        if (regex.isEmpty()) {
+          throw new ExecutionException("No tests to run");
+        }
+        builder.append(" -n \"").append(regex).append("\"");
+      }
+    }
+
+    params.setArguments(builder.toString());
+    // working directory is not configurable in UI because there's only one valid value that we calculate ourselves
+    params.setWorkingDirectory(params.computeProcessWorkingDirectory(project));
+    return doStartProcess(pathToDartUrl(sdk.getHomePath() + PUB_SNAPSHOT_PATH));
+  }
+
+  DartTestRunnerParameters getParameters() {
+    return (DartTestRunnerParameters)myRunnerParameters;
+  }
+
+  private static String pathToDartUrl(@NonNls @NotNull String path) {
+    final String url = VfsUtilCore.pathToUrl(path);
+    return SystemInfo.isWindows ? url.replace("file://", "file:///") : url;
+  }
+
+  private static class DartConsoleProperties extends SMTRunnerConsoleProperties implements SMCustomMessagesParsing {
+    public DartConsoleProperties(DartRunConfiguration runConfiguration, ExecutionEnvironment env) {
+      super(runConfiguration, DART_FRAMEWORK_NAME, env.getExecutor());
+      setUsePredefinedMessageFilter(false);
+      setIdBasedTestTree(true);
+    }
+
+    @Nullable
+    @Override
+    public SMTestLocator getTestLocator() {
+      return DartTestLocationProvider.INSTANCE;
+    }
+
+    @Override
+    public OutputToGeneralTestEventsConverter createTestEventsConverter(@NotNull String testFrameworkName,
+                                                                        @NotNull TestConsoleProperties consoleProperties) {
+      final DartRunConfiguration runConfiguration = (DartRunConfiguration)getConfiguration();
+      try {
+        final VirtualFile file = runConfiguration.getRunnerParameters().getDartFileOrDirectory();
+        return new DartTestEventsConverter(testFrameworkName, consoleProperties, DartUrlResolver.getInstance(getProject(), file));
+      }
+      catch (RuntimeConfigurationError error) {
+        throw new RuntimeException(error); // can't happen, already checked
+      }
+    }
+
+    @Nullable
+    @Override
+    public AbstractRerunFailedTestsAction createRerunFailedTestsAction(ConsoleView consoleView) {
+      if (ActionManager.getInstance().getAction("RerunFailedTests") == null) return null; // backward compatibility
+
+      DartTestRerunnerAction action = new DartTestRerunnerAction(consoleView);
+      action.init(this);
+      return action;
+    }
+  }
+}
