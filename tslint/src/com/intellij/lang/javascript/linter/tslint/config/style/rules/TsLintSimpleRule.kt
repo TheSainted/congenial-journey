62071abe7fa462ce104968e0b1281b85527b500b
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:40:49 -0700

    Initialize ypankratyev/test_data_fix

---
+++ tslint/src/com/intellij/lang/javascript/linter/tslint/config/style/rules/TsLintSimpleRule.kt
@@ -0,0 +1,37 @@
+package com.intellij.lang.javascript.linter.tslint.config.style.rules
+
+import com.intellij.lang.javascript.formatter.JSCodeStyleSettings
+import com.intellij.openapi.project.Project
+import com.intellij.psi.codeStyle.CommonCodeStyleSettings
+
+
+abstract class TsLintSimpleRule<T>(val optionId: String) : TsLintRule {
+  override fun isAvailable(project: Project,
+                           languageSettings: CommonCodeStyleSettings,
+                           codeStyleSettings: JSCodeStyleSettings,
+                           config: TsLintConfigWrapper): Boolean {
+    if (!hasOption(config)) return false
+
+    val configValue = getConfigValue(config)
+    return configValue != null && getSettingsValue(languageSettings, codeStyleSettings) != configValue
+  }
+
+  abstract fun getConfigValue(config: TsLintConfigWrapper): T?
+
+  override fun apply(project: Project,
+                     languageSettings: CommonCodeStyleSettings,
+                     codeStyleSettings: JSCodeStyleSettings,
+                     config: TsLintConfigWrapper) {
+    val value = getConfigValue(config) ?: return
+
+    setValue(languageSettings, codeStyleSettings, value)
+  }
+
+  open fun hasOption(config: TsLintConfigWrapper): Boolean = config.getOption(optionId)?.isTrue() ?: false
+
+  abstract fun getSettingsValue(languageSettings: CommonCodeStyleSettings,
+                                codeStyleSettings: JSCodeStyleSettings): T
+
+  abstract fun setValue(languageSettings: CommonCodeStyleSettings,
+                        codeStyleSettings: JSCodeStyleSettings, value: T)
+}
