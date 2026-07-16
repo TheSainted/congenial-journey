93133c432797a01b3f08d5c526d3abe9162e36fb
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:40:52 -0700

    Initialize akoshevoy/fixPythonConsoleForDocker

---
+++ Dart/src/com/jetbrains/lang/dart/flutter/FlutterUtil.java
@@ -0,0 +1,31 @@
+package com.jetbrains.lang.dart.flutter;
+
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.openapi.extensions.PluginId;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class FlutterUtil {
+
+  private static final String FLUTTER_MODULE_TYPE_ID = "FLUTTER_MODULE_TYPE";
+  private static final boolean FLUTTER_PLUGIN_INSTALLED = PluginManager.isPluginInstalled(PluginId.getId("io.flutter"));
+
+  /**
+   * @return the Flutter SDK root relative to the given Dart SDK or {@code null}
+   */
+  @Nullable
+  public static String getFlutterRoot(@NotNull final String dartSdkPath) {
+    final String suffix = "/bin/cache/dart-sdk";
+    return dartSdkPath.endsWith(suffix) ? dartSdkPath.substring(0, dartSdkPath.length() - suffix.length()) : null;
+  }
+
+  public static boolean isFlutterModule(@NotNull final Module module) {
+    return FLUTTER_MODULE_TYPE_ID.equals(ModuleType.get(module).getId());
+  }
+
+  public static boolean isFlutterPluginInstalled() {
+    return FLUTTER_PLUGIN_INSTALLED;
+  }
+}
