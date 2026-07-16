6bf1758be15f7f9d7843af17177b1218ea416a40
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:22:20 +0000

    Initialize avokin/handling-torn-sm-strings

---
+++ AngularJS/src/org/angular2/codeInsight/tags/Angular2TagDescriptor.java
@@ -0,0 +1,106 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.angular2.codeInsight.tags;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.impl.source.html.dtd.HtmlNSDescriptorImpl;
+import com.intellij.psi.impl.source.xml.XmlDescriptorUtil;
+import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.xml.XmlAttributeDescriptor;
+import com.intellij.xml.XmlElementDescriptor;
+import com.intellij.xml.XmlElementsGroup;
+import com.intellij.xml.XmlNSDescriptor;
+import org.angular2.codeInsight.attributes.Angular2AttributeDescriptorsProvider;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class Angular2TagDescriptor implements XmlElementDescriptor {
+  private final String myName;
+  private final PsiElement myDeclaration;
+
+  public Angular2TagDescriptor(String name, @NotNull PsiElement declaration) {
+    myName = name;
+    myDeclaration = declaration;
+  }
+
+  @Override
+  public String getQualifiedName() {
+    return myName;
+  }
+
+  @Override
+  public String getDefaultName() {
+    return myName;
+  }
+
+  @Override
+  public XmlElementDescriptor[] getElementsDescriptors(XmlTag context) {
+    return XmlDescriptorUtil.getElementsDescriptors(context);
+  }
+
+  @Override
+  public XmlElementDescriptor getElementDescriptor(XmlTag childTag, XmlTag contextTag) {
+    return XmlDescriptorUtil.getElementDescriptor(childTag, contextTag);
+  }
+
+  @Override
+  public XmlAttributeDescriptor[] getAttributesDescriptors(@Nullable XmlTag context) {
+    return HtmlNSDescriptorImpl.getCommonAttributeDescriptors(context);
+  }
+
+  @Nullable
+  @Override
+  public XmlAttributeDescriptor getAttributeDescriptor(XmlAttribute attribute) {
+    return getAttributeDescriptor(attribute.getName(), attribute.getParent());
+  }
+
+  @Nullable
+  @Override
+  public XmlAttributeDescriptor getAttributeDescriptor(@NonNls final String attributeName, @Nullable XmlTag context) {
+    return Angular2AttributeDescriptorsProvider.getAttributeDescriptor(
+      attributeName, context, this::getAttributesDescriptors);
+  }
+
+  @Override
+  public XmlNSDescriptor getNSDescriptor() {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public XmlElementsGroup getTopGroup() {
+    return null;
+  }
+
+  @Override
+  public int getContentType() {
+    return CONTENT_TYPE_ANY;
+  }
+
+  @Nullable
+  @Override
+  public String getDefaultValue() {
+    return null;
+  }
+
+  @Override
+  @NotNull
+  public PsiElement getDeclaration() {
+    return myDeclaration;
+  }
+
+  @Override
+  public String getName(PsiElement context) {
+    return getName();
+  }
+
+  @Override
+  public String getName() {
+    return myName;
+  }
+
+  @Override
+  public void init(PsiElement element) {
+  }
+}
