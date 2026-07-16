d7ba34b11c6352342680970aed1ea62b8941de48
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:19:49 -0700

    Initialize 171.2014_ruby

---
+++ js-karma/src/com/intellij/javascript/karma/execution/KarmaServerSettings.java
@@ -0,0 +1,117 @@
+package com.intellij.javascript.karma.execution;
+
+import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreter;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+public class KarmaServerSettings {
+
+  private final NodeJsLocalInterpreter myNodeInterpreter;
+  private final String myKarmaPackageDirPath;
+  private final KarmaRunSettings myRunSettings;
+  private final boolean myWithCoverage;
+
+  private KarmaServerSettings(@NotNull Builder builder) {
+    myNodeInterpreter = builder.myNodeInterpreter;
+    myKarmaPackageDirPath = builder.myKarmaPackageDirPath;
+    myRunSettings = builder.myRunSettings;
+    myWithCoverage = builder.myWithCoverage;
+  }
+
+  @NotNull
+  public NodeJsLocalInterpreter getNodeInterpreter() {
+    return myNodeInterpreter;
+  }
+
+  @NotNull
+  public File getKarmaPackageDir() {
+    return new File(myKarmaPackageDirPath);
+  }
+
+  @NotNull
+  public KarmaRunSettings getRunSettings() {
+    return myRunSettings;
+  }
+
+  public boolean isWithCoverage() {
+    return myWithCoverage;
+  }
+
+  @NotNull
+  public File getConfigurationFile() {
+    return new File(myRunSettings.getConfigPath());
+  }
+
+  @NotNull
+  public String getConfigurationFilePath() {
+    return myRunSettings.getConfigPath();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    KarmaServerSettings that = (KarmaServerSettings)o;
+
+    return myKarmaPackageDirPath.equals(that.myKarmaPackageDirPath) &&
+           myNodeInterpreter.getInterpreterSystemIndependentPath().equals(that.myNodeInterpreter.getInterpreterSystemIndependentPath()) &&
+           myRunSettings.equals(that.myRunSettings) &&
+           myWithCoverage == that.myWithCoverage;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = myNodeInterpreter.getInterpreterSystemIndependentPath().hashCode();
+    result = 31 * result + myKarmaPackageDirPath.hashCode();
+    result = 31 * result + myRunSettings.hashCode();
+    result = 31 * result + (myWithCoverage ? 1 : 0);
+    return result;
+  }
+
+  public static class Builder {
+    private NodeJsLocalInterpreter myNodeInterpreter;
+    private String myKarmaPackageDirPath;
+    private KarmaRunSettings myRunSettings;
+    private boolean myWithCoverage;
+
+    @NotNull
+    public Builder setNodeInterpreter(@NotNull NodeJsLocalInterpreter interpreter) {
+      myNodeInterpreter = interpreter;
+      return this;
+    }
+
+    @NotNull
+    public Builder setKarmaPackageDirPath(@NotNull String karmaPackageDirPath) {
+      myKarmaPackageDirPath = karmaPackageDirPath;
+      return this;
+    }
+
+    @NotNull
+    public Builder setRunSettings(@NotNull KarmaRunSettings runSettings) {
+      myRunSettings = runSettings;
+      return this;
+    }
+
+    @NotNull
+    public Builder setWithCoverage(boolean withCoverage) {
+      myWithCoverage = withCoverage;
+      return this;
+    }
+
+    @NotNull
+    public KarmaServerSettings build() {
+      if (myNodeInterpreter == null) {
+        throw new RuntimeException("Unspecified Node.js interpreter");
+      }
+      if (myKarmaPackageDirPath == null) {
+        throw new RuntimeException("Path to karma package isn't set!");
+      }
+      if (myRunSettings == null) {
+        throw new RuntimeException("Run settings aren't set!");
+      }
+      return new KarmaServerSettings(this);
+    }
+  }
+}
