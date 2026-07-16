e8df13ea90a98b13711231b05450e01d0d9f94ba
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:01:49 +0000

    Initialize ostrizhe/extension_category_enum

---
+++ Dart/src/com/jetbrains/lang/dart/psi/DartExpressionCodeFragment.java
@@ -0,0 +1,9 @@
+package com.jetbrains.lang.dart.psi;
+
+import com.intellij.psi.PsiCodeFragment;
+
+/**
+ * @author: Fedor.Korotkov
+ */
+public interface DartExpressionCodeFragment extends PsiCodeFragment {
+}
