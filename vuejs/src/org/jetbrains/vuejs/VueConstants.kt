a31f681246286d2d51abae74385b78e55817b373
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:58:36 -0700

    Initialize roman_ivanov/try_finally

---
+++ vuejs/src/org/jetbrains/vuejs/VueConstants.kt
@@ -0,0 +1,8 @@
+package org.jetbrains.vuejs
+
+/**
+ * @author Irina.Chernushina on 10/5/2017.
+ */
+const val MIXINS = "mixins"
+const val DIRECTIVES = "directives"
+const val GLOBAL_BINDING_MARK = "*"
