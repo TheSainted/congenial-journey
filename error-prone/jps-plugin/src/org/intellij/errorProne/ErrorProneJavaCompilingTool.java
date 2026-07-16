283c89cc993200a7def6f9491e7c185b8fa3e45b
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 04:46:46 +0000

    Initialize smelov/WI-40977

---
+++ error-prone/jps-plugin/src/org/intellij/errorProne/ErrorProneJavaCompilingTool.java
@@ -0,0 +1,48 @@
+package org.intellij.errorProne;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.jps.builders.java.CannotCreateJavaCompilerException;
+import org.jetbrains.jps.builders.java.JavaCompilingTool;
+
+import javax.tools.*;
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author nik
+ */
+public class ErrorProneJavaCompilingTool extends JavaCompilingTool {
+  public static final String COMPILER_ID = "error-prone";//duplicates ErrorProneJavaBackendCompiler.COMPILER_ID from IDE part
+  private static final String VERSION_PROPERTY = "idea.error.prone.version";//duplicates ErrorProneClasspathProvider.VERSION_PROPERTY
+
+  @NotNull
+  @Override
+  public String getId() {
+    return COMPILER_ID;
+  }
+
+  @NotNull
+  @Override
+  public String getDescription() {
+    String version = System.getProperty(VERSION_PROPERTY);
+    return "error-prone compiler " + (version != null ? version : "(unknown version)");
+  }
+
+  @NotNull
+  @Override
+  public JavaCompiler createCompiler() throws CannotCreateJavaCompilerException {
+    try {
+      return (JavaCompiler)Class.forName("com.google.errorprone.ErrorProneJavaCompiler").newInstance();
+    }
+    catch (Exception e) {
+      throw new CannotCreateJavaCompilerException(e.getMessage());
+    }
+  }
+
+  @NotNull
+  @Override
+  public List<File> getAdditionalClasspath() {
+    return Collections.emptyList();
+  }
+}
