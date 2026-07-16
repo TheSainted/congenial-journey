00bb090a234bd9915856b6d4c509ee4d163f7459
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:30:21 -0700

    Initialize path_logging

---
+++ tslint/src/com/intellij/lang/javascript/linter/tslint/config/TsLintDescriptor.java
@@ -0,0 +1,30 @@
+package com.intellij.lang.javascript.linter.tslint.config;
+
+import com.intellij.lang.javascript.linter.JSLinterConfigFileUtil;
+import com.intellij.lang.javascript.linter.JSLinterConfiguration;
+import com.intellij.lang.javascript.linter.JSLinterDescriptor;
+import com.intellij.lang.javascript.linter.tslint.ide.TsLintConfigFileType;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Irina.Chernushina on 11/24/2016.
+ */
+public final class TsLintDescriptor extends JSLinterDescriptor {
+
+  @Override
+  public String packageName() {
+    return "tslint";
+  }
+
+  @Override
+  public boolean hasConfigFiles(@NotNull Project project) {
+    return JSLinterConfigFileUtil.projectHasConfigFiles(project, TsLintConfigFileType.INSTANCE);
+  }
+
+  @NotNull
+  @Override
+  public Class<? extends JSLinterConfiguration> getConfigurationClass() {
+    return TsLintConfiguration.class;
+  }
+}
