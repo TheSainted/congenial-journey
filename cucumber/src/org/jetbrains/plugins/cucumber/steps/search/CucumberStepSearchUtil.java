05ea33a6db7212440071cbc7eb6e873ab376d4b2
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:31:22 -0700

    Initialize cfmlSqlInjection

---
+++ cucumber/src/org/jetbrains/plugins/cucumber/steps/search/CucumberStepSearchUtil.java
@@ -0,0 +1,24 @@
+package org.jetbrains.plugins.cucumber.steps.search;
+
+import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.util.Computable;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.SearchScope;
+import org.jetbrains.plugins.cucumber.psi.GherkinFileType;
+
+/**
+ * User: Andrey.Vokin
+ * Date: 7/25/12
+ */
+public class CucumberStepSearchUtil {
+  public static SearchScope restrictScopeToGherkinFiles(final Computable<SearchScope> originalScopeComputation) {
+    return ReadAction.compute(() -> {
+      final SearchScope originalScope = originalScopeComputation.compute();
+      if (originalScope instanceof GlobalSearchScope) {
+        return GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)originalScope,
+                                                               GherkinFileType.INSTANCE);
+      }
+      return originalScope;
+    });
+  }
+}
