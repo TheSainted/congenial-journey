7eb9ad189384daa301a3ecccfede9016acc37fbf
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 19:58:45 -0700

    Initialize pandas-viewer

---
+++ Dart/src/com/jetbrains/lang/dart/ide/hierarchy/DartHierarchyUtil.java
@@ -0,0 +1,71 @@
+package com.jetbrains.lang.dart.ide.hierarchy;
+
+import com.intellij.ide.hierarchy.HierarchyBrowserManager;
+import com.intellij.ide.util.treeView.AlphaComparator;
+import com.intellij.ide.util.treeView.NodeDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.jetbrains.lang.dart.DartComponentType;
+import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
+import com.jetbrains.lang.dart.ide.marker.DartServerOverrideMarkerProvider;
+import com.jetbrains.lang.dart.psi.DartClass;
+import com.jetbrains.lang.dart.psi.DartComponent;
+import com.jetbrains.lang.dart.psi.DartComponentName;
+import org.dartlang.analysis.server.protocol.Element;
+import org.dartlang.analysis.server.protocol.Location;
+import org.dartlang.analysis.server.protocol.TypeHierarchyItem;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class DartHierarchyUtil {
+  private static final Comparator<NodeDescriptor> NODE_DESCRIPTOR_COMPARATOR = (first, second) -> first.getIndex() - second.getIndex();
+
+  private DartHierarchyUtil() {
+  }
+
+  @Nullable
+  public static DartClass findDartClass(@NotNull final Project project, @NotNull final TypeHierarchyItem item) {
+    final Element classElement = item.getClassElement();
+    final Location location = classElement.getLocation();
+    final DartComponent component = DartServerOverrideMarkerProvider.findDartComponent(project, location);
+    return component instanceof DartClass ? (DartClass)component : null;
+  }
+
+  public static Comparator<NodeDescriptor> getComparator(Project project) {
+    if (HierarchyBrowserManager.getInstance(project).getState().SORT_ALPHABETICALLY) {
+      return AlphaComparator.INSTANCE;
+    }
+    else {
+      return NODE_DESCRIPTOR_COMPARATOR;
+    }
+  }
+
+  @NotNull
+  public static List<TypeHierarchyItem> getTypeHierarchyItems(@NotNull DartClass dartClass) {
+    final VirtualFile file = dartClass.getContainingFile().getVirtualFile();
+    final DartComponentName name = dartClass.getComponentName();
+    if (name == null) return Collections.emptyList();
+
+    return DartAnalysisServerService.getInstance().search_getTypeHierarchy(file, name.getTextRange().getStartOffset(), false);
+  }
+
+  public static boolean isExecutable(@NotNull PsiElement component) {
+    if (component instanceof DartComponentName) return false;
+    final DartComponentType componentType = DartComponentType.typeOf(component);
+    if (componentType == null) return false;
+    switch (componentType) {
+      case CONSTRUCTOR:
+      case FUNCTION:
+      case METHOD:
+      case OPERATOR:
+        return true;
+      default:
+        return false;
+    }
+  }
+}
