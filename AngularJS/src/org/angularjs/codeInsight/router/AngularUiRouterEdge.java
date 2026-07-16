238155cd0dd321278c2f942973b3a270de66745f
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 19:59:16 -0700

    Initialize PY17535

---
+++ AngularJS/src/org/angularjs/codeInsight/router/AngularUiRouterEdge.java
@@ -0,0 +1,90 @@
+package org.angularjs.codeInsight.router;
+
+import com.intellij.diagram.DiagramEdgeBase;
+import com.intellij.diagram.DiagramNode;
+import com.intellij.diagram.DiagramRelationshipInfo;
+import com.intellij.diagram.DiagramRelationshipInfoAdapter;
+import com.intellij.diagram.presentation.DiagramLineType;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
+
+/**
+ * @author Irina.Chernushina on 3/23/2016.
+ */
+public class AngularUiRouterEdge extends DiagramEdgeBase<DiagramObject> {
+  private final static DiagramRelationshipInfo DEPENDS = new DiagramRelationshipInfoAdapter("DEPENDS", DiagramLineType.SOLID) {
+    @Override
+    public Shape getStartArrow() {
+      return NONE;
+    }
+
+    @Override
+    public Shape getEndArrow() {
+      return STANDARD;
+    }
+  };
+
+  private final String myLabel;
+  private final Type myType;
+  @Nullable
+  private String mySourceName;
+  @Nullable
+  private String myTargetName;
+
+  public AngularUiRouterEdge(DiagramNode<DiagramObject> source,
+                             DiagramNode<DiagramObject> target,
+                             final String label,
+                             Type type) {
+    super(source, target, new DiagramRelationshipInfoAdapter("BUILTIN", DiagramLineType.SOLID) {
+      @Override
+      public String getLabel() {
+        return "";
+      }
+
+      @Override
+      public Shape getStartArrow() {
+        return NONE;
+      }
+
+      @Override
+      public Shape getEndArrow() {
+        return STANDARD;
+      }
+    });
+    myLabel = label;
+    myType = type;
+  }
+
+  @Nullable
+  public String getSourceName() {
+    return mySourceName;
+  }
+
+  public AngularUiRouterEdge setSourceName(@Nullable String sourceName) {
+    mySourceName = sourceName;
+    return this;
+  }
+
+  @Nullable
+  public String getTargetName() {
+    return myTargetName;
+  }
+
+  public String getLabel() {
+    return myLabel;
+  }
+
+  public AngularUiRouterEdge setTargetName(@Nullable String targetName) {
+    myTargetName = targetName;
+    return this;
+  }
+
+  public Type getType() {
+    return myType;
+  }
+
+  public static enum Type {
+    providesTemplate, fillsTemplate, parent
+  }
+}
---
+++ AngularJS/src/org/angularjs/codeInsight/router/ShowUiRouterStatesNewDiagramAction.java
@@ -0,0 +1,103 @@
+package org.angularjs.codeInsight.router;
+
+import com.intellij.diagram.DiagramProvider;
+import com.intellij.internal.statistic.UsageTrigger;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.registry.Registry;
+import com.intellij.openapi.util.registry.RegistryValue;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.components.JBList;
+import com.intellij.uml.core.actions.ShowDiagram;
+import com.intellij.util.ArrayUtil;
+import com.intellij.util.Consumer;
+import icons.AngularJSIcons;
+import org.angularjs.index.AngularIndexUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Irina.Chernushina on 3/23/2016.
+ */
+public class ShowUiRouterStatesNewDiagramAction extends ShowDiagram {
+  public static final String USAGE_KEY = "angular.js.ui.router.show.diagram";
+  private VirtualFile myCurrentRootFile;
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    UsageTrigger.trigger(USAGE_KEY);
+    final Project project = e.getProject();
+    if (project == null) return;
+
+    // todo read action?
+    final AngularUiRouterDiagramBuilder builder = new AngularUiRouterDiagramBuilder(project);
+    builder.build();
+    List<Pair<String, AngularUiRouterGraphBuilder>> graphBuilders = new ArrayList<>();
+    final Map<VirtualFile, RootTemplate> rootTemplates = builder.getRootTemplates();
+
+    for (Map.Entry<VirtualFile, Map<String, UiRouterState>> entry : builder.getDefiningFiles2States().entrySet()) {
+      final AngularUiRouterGraphBuilder graphBuilder =
+        new AngularUiRouterGraphBuilder(project, entry.getValue(), builder.getTemplatesMap(), null, entry.getKey());
+      graphBuilders.add(Pair.create(entry.getKey().getName(), graphBuilder));
+    }
+    for (Map.Entry<VirtualFile, Map<String, UiRouterState>> entry : builder.getRootTemplates2States().entrySet()) {
+      final AngularUiRouterGraphBuilder graphBuilder =
+        new AngularUiRouterGraphBuilder(project, entry.getValue(), builder.getTemplatesMap(), rootTemplates.get(entry.getKey()),
+                                        entry.getKey());
+      graphBuilders.add(Pair.create(entry.getKey().getName(), graphBuilder));
+    }
+    // todo look on mac
+    final AngularUiRouterDiagramProvider diagramProvider =
+      (AngularUiRouterDiagramProvider)DiagramProvider.findByID(AngularUiRouterDiagramProvider.ANGULAR_UI_ROUTER);
+    if (diagramProvider == null) return;
+
+    diagramProvider.reset();
+    final Consumer<AngularUiRouterGraphBuilder> consumer = graphBuilder -> {
+      myCurrentRootFile = graphBuilder.getKey();
+      final AngularUiRouterGraphBuilder.GraphNodesBuilder nodesBuilder = graphBuilder.createDataModel(diagramProvider);
+      diagramProvider.registerNodesBuilder(nodesBuilder);
+      final Runnable callback =
+        show(nodesBuilder.getRootNode().getIdentifyingElement(), diagramProvider, project, null, Collections.emptyList());
+      if (callback != null) {
+        callback.run();
+      }
+    };
+    if (graphBuilders.size() == 1) consumer.consume(graphBuilders.get(0).getSecond());
+    else filterGraphBuilders(project, graphBuilders, consumer);
+  }
+
+  private static void filterGraphBuilders(Project project, List<Pair<String, AngularUiRouterGraphBuilder>> builders,
+                                                                                     Consumer<AngularUiRouterGraphBuilder> consumer) {
+    final JBList list = new JBList();
+    final List<Object> data = new ArrayList<>();
+    for (Pair<String, AngularUiRouterGraphBuilder> builder : builders) {
+      data.add(builder.getSecond().getKey().getPath());
+    }
+    list.setListData(ArrayUtil.toObjectArray(data));
+    JBPopupFactory.getInstance().createListPopupBuilder(list)
+      .setTitle("Select Main Template File")
+      .setItemChoosenCallback(() -> {
+        final int index = list.getSelectedIndex();
+        if (index >= 0) {
+          consumer.consume(builders.get(index).getSecond());
+        }
+      })
+      .createPopup().showCenteredInCurrentWindow(project);
+  }
+
+  @Override
+  public void update(AnActionEvent e) {
+    final Project project = e.getProject();
+    final RegistryValue value = Registry.get("angular.js.ui.router.diagram");
+    e.getPresentation().setEnabled(value.isBoolean() && value.asBoolean() && project != null && AngularIndexUtil.hasAngularJS(project));
+
+    e.getPresentation().setText("Show ui-router State Diagram");
+    e.getPresentation().setDescription("Show ui-router State Diagram");
+    e.getPresentation().setIcon(AngularJSIcons.AngularJS);
+  }
+}
