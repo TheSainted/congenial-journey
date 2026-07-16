84e854b85a5107f4d4796fefba31589864a9af78
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:37:01 -0700

    Initialize WI-15215_Hierarchy_check

---
+++ JsTestDriver/src/com/google/jstestdriver/idea/coverage/JstdCoverageAnnotator.java
@@ -0,0 +1,43 @@
+package com.google.jstestdriver.idea.coverage;
+
+import com.intellij.coverage.SimpleCoverageAnnotator;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+
+/**
+ * @author Sergey Simonchik
+ */
+public class JstdCoverageAnnotator extends SimpleCoverageAnnotator {
+
+  public JstdCoverageAnnotator(Project project) {
+    super(project);
+  }
+
+  public static JstdCoverageAnnotator getInstance(@NotNull Project project) {
+    return ServiceManager.getService(project, JstdCoverageAnnotator.class);
+  }
+
+  @Override
+  @Nullable
+  protected FileCoverageInfo fillInfoForUncoveredFile(@NotNull File file) {
+    return null;
+  }
+
+  @Override
+  protected String getLinesCoverageInformationString(@NotNull FileCoverageInfo info) {
+    if (info.totalLineCount == 0) {
+      return null;
+    }
+    if (info.coveredLineCount == 0) {
+      return "not covered";
+    }
+    else {
+      return calcCoveragePercentage(info) + "% lines covered";
+    }
+  }
+
+}
