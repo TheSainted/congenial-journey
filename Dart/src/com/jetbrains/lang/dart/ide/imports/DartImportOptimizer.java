611f2d00d5114342201f90ad4cdba88e71522d14
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:15:26 +0000

    Initialize 183

---
+++ Dart/src/com/jetbrains/lang/dart/ide/imports/DartImportOptimizer.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetbrains.lang.dart.ide.imports;
+
+import com.intellij.lang.ImportOptimizer;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.jetbrains.lang.dart.DartBundle;
+import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
+import com.jetbrains.lang.dart.assists.AssistUtils;
+import com.jetbrains.lang.dart.psi.DartFile;
+import com.jetbrains.lang.dart.util.DartResolveUtil;
+import org.dartlang.analysis.server.protocol.SourceFileEdit;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class DartImportOptimizer implements ImportOptimizer {
+  @NotNull
+  @Override
+  public Runnable processFile(final PsiFile file) {
+    DartAnalysisServerService.getInstance(file.getProject()).serverReadyForRequest(file.getProject());
+    return new CollectingInfoRunnable() {
+      private boolean myFileChanged = false;
+
+      @Override
+      public void run() {
+        final VirtualFile vFile = DartResolveUtil.getRealVirtualFile(file);
+        if (vFile != null) {
+          final String filePath = vFile.getPath();
+          final SourceFileEdit fileEdit = DartAnalysisServerService.getInstance(file.getProject()).edit_organizeDirectives(filePath);
+          if (fileEdit != null) {
+            if (AssistUtils.applyFileEdit(file.getProject(), fileEdit)) {
+              myFileChanged = true;
+              final Document document = PsiDocumentManager.getInstance(file.getProject()).getDocument(file);
+              if (document != null) {
+                // Tricky story. Committing a document here is required in order to guarantee that DartPostFormatProcessor.processText() is called afterwards.
+                PsiDocumentManager.getInstance(file.getProject()).commitDocument(document);
+              }
+            }
+          }
+        }
+      }
+
+      @Nullable
+      @Override
+      public String getUserNotificationInfo() {
+        return myFileChanged ? DartBundle.message("organized.directives") : null;
+      }
+    };
+  }
+
+  @Override
+  public boolean supports(PsiFile file) {
+    return file instanceof DartFile;
+  }
+}
