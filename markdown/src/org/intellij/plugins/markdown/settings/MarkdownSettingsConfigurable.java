fd3d81c96ca31738119508060db5ba011bf422a1
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:43:36 -0700

    Initialize mekh_art/mixed-debugger

---
+++ markdown/src/org/intellij/plugins/markdown/settings/MarkdownSettingsConfigurable.java
@@ -0,0 +1,77 @@
+package org.intellij.plugins.markdown.settings;
+
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.SearchableConfigurable;
+import com.intellij.openapi.util.Disposer;
+import org.intellij.plugins.markdown.MarkdownBundle;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class MarkdownSettingsConfigurable implements SearchableConfigurable {
+  @Nullable
+  private MarkdownSettingsForm myForm = null;
+  @NotNull
+  private MarkdownApplicationSettings myMarkdownApplicationSettings;
+
+  public MarkdownSettingsConfigurable(@NotNull MarkdownApplicationSettings markdownApplicationSettings) {
+    myMarkdownApplicationSettings = markdownApplicationSettings;
+  }
+
+  @NotNull
+  @Override
+  public String getId() {
+    return "Settings.Markdown";
+  }
+
+  @Nls
+  @Override
+  public String getDisplayName() {
+    return MarkdownBundle.message("settings.markdown.name");
+  }
+
+  @NotNull
+  @Override
+  public JComponent createComponent() {
+    return getForm().getComponent();
+  }
+
+  @NotNull
+  public MarkdownSettingsForm getForm() {
+    if (myForm == null) {
+      myForm = new MarkdownSettingsForm();
+    }
+    return myForm;
+  }
+
+  @Override
+  public boolean isModified() {
+    return !getForm().getMarkdownCssSettings().equals(myMarkdownApplicationSettings.getMarkdownCssSettings()) ||
+           !getForm().getMarkdownPreviewSettings().equals(myMarkdownApplicationSettings.getMarkdownPreviewSettings());
+  }
+
+  @Override
+  public void apply() throws ConfigurationException {
+    final MarkdownSettingsForm form = getForm();
+
+    form.validate();
+    myMarkdownApplicationSettings.setMarkdownCssSettings(form.getMarkdownCssSettings());
+    myMarkdownApplicationSettings.setMarkdownPreviewSettings(form.getMarkdownPreviewSettings());
+  }
+
+  @Override
+  public void reset() {
+    getForm().setMarkdownCssSettings(myMarkdownApplicationSettings.getMarkdownCssSettings());
+    getForm().setMarkdownPreviewSettings(myMarkdownApplicationSettings.getMarkdownPreviewSettings());
+  }
+
+  @Override
+  public void disposeUIResources() {
+    if (myForm != null) {
+      Disposer.dispose(myForm);
+    }
+    myForm = null;
+  }
+}
