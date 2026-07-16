cb57d46508fc6e82a53ceec9f6468c05e78952f2
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:14:08 +0000

    Initialize amp/vcs-mappings-project-rework-7

---
+++ postcss/src/org/intellij/plugins/postcss/psi/impl/PostCssTreeElementFactory.java
@@ -0,0 +1,28 @@
+package org.intellij.plugins.postcss.psi.impl;
+
+import com.intellij.psi.css.impl.CssTreeElementFactory;
+import com.intellij.psi.impl.source.tree.CompositeElement;
+import com.intellij.psi.tree.IElementType;
+import org.intellij.plugins.postcss.PostCssElementTypes;
+import org.jetbrains.annotations.NotNull;
+
+public class PostCssTreeElementFactory extends CssTreeElementFactory {
+
+  @Override
+  @NotNull
+  public CompositeElement createComposite(final IElementType type) {
+    if (type == PostCssElementTypes.POST_CSS_NEST) {
+      return new PostCssNestImpl();
+    }
+    else if (type == PostCssElementTypes.POST_CSS_CUSTOM_SELECTOR_RULE) {
+      return new PostCssCustomSelectorAtRuleImpl();
+    }
+    else if (type == PostCssElementTypes.POST_CSS_CUSTOM_MEDIA_RULE) {
+      return new PostCssCustomMediaAtRuleImpl();
+    }
+    else if (type == PostCssElementTypes.POST_CSS_SIMPLE_VARIABLE_DECLARATION) {
+      return new PostCssSimpleVariableDeclarationImpl();
+    }
+    return super.createComposite(type);
+  }
+}
