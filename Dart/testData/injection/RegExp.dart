b355761c358aaa7f6e7d9333277fb86b8c15506c
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:48:43 -0700

    Initialize vromanik/wsl_util

---
+++ Dart/testData/injection/RegExp.dart
@@ -0,0 +1,13 @@
+var a = new RegExp(r'');
+var a = new RegExp(r'.');
+var a = new RegExp(r".");
+var a = new RegExp(r'''.''');
+var a = new RegExp(r""".""");
+var a = new RegExp('.');
+var a = new RegExp(".");
+var a = new RegEx(r'.');
+var a = new RegEx(r".");
+var a = RegExp(r'.');
+var a = RegExp(r".");
+var a = new RegExp(r'.' r".");
+final _specialChars = new RegExp(r'[\\()$^.+[\]{}|]');
