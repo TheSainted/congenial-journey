10b70f915cd08801ccfe3e67ba1bf27b36b381d2
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:17:02 +0000

    Initialize 192.5438

---
+++ cucumber/test/org/jetbrains/plugins/cucumber/psi/JsonGherkinKeywordProviderTest.java
@@ -0,0 +1,18 @@
+package org.jetbrains.plugins.cucumber.psi;
+
+import org.jetbrains.plugins.cucumber.CucumberTestUtil;
+import org.jetbrains.plugins.cucumber.i18n.AbstractGherkinKeywordProviderTest;
+import org.jetbrains.plugins.cucumber.psi.i18n.JsonGherkinKeywordProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class JsonGherkinKeywordProviderTest extends AbstractGherkinKeywordProviderTest {
+  @Override
+  protected GherkinKeywordProvider buildKeywordProvider() {
+    ClassLoader classLoader = JsonGherkinKeywordProvider.class.getClassLoader();
+    InputStream inputStream = classLoader.getResourceAsStream("i18n.json");
+    return new JsonGherkinKeywordProvider(inputStream);
+  }
+}
