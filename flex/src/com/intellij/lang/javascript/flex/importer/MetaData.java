dbfa5619237ae6ba0ff977f6e3507cef42639cab
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:14:00 +0000

    Initialize roman_ivanov/hints

---
+++ flex/src/com/intellij/lang/javascript/flex/importer/MetaData.java
@@ -0,0 +1,10 @@
+package com.intellij.lang.javascript.flex.importer;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author Maxim.Mossienko
+*/
+class MetaData extends LinkedHashMap<String, String> {
+  String name;
+}
