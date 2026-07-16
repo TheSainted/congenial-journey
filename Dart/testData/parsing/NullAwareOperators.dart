b799f15c70cdb5b0adc76dfe56b4d3ba7e751225
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:19:03 +0000

    Initialize 192-epeshk-rider-implementationview

---
+++ Dart/testData/parsing/NullAwareOperators.dart
@@ -0,0 +1,34 @@
+class NAO1 {
+  f(x,y,z) {
+    var a = (x)?.y?.[b];
+    var b = x?.y;
+    var c = x?.z();
+    var e = a ?? b ? y : z;
+    var f = x ?? y ?? z, g, h;
+    e = a ?? b ? y : z;
+    f = x ?? y ?? z;
+    g = x || y ?? z;
+    h = x ?? y || z;
+    a ??= c;
+    a ||= c;
+    a &&= c;
+    this?.f(e, f, g ?? h);
+
+    aaa!.bcc!()! as CCC ..ddd()!![][]![]!();
+  }
+}
+
+fnao1(x) => x?.op();
+fnao2(x) => x ?? "other";
+fnao3(x) {
+  b? c;
+  b? c = d;
+  b? c:d;
+  e = f ? g : h;
+
+  var y;
+  y ??= x;
+  return y;
+}
+
+static final Map<String?, Map<int?, bool?>?>? a;
