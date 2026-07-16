aa9451b64fb5cb14d22d27ba0aef413beb27f528
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:13:01 +0000

    Initialize east825/python-quotes-handling-improvements

---
+++ prettierJS/src/com/intellij/prettierjs/PrettierLanguageService.java
@@ -0,0 +1,58 @@
+package com.intellij.prettierjs;
+
+import com.intellij.javascript.nodejs.util.NodePackage;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+public interface PrettierLanguageService {
+  @Nullable
+  CompletableFuture<FormatResult> format(@NotNull String filePath,
+                                         String ignoreFilePath, @NotNull String text,
+                                         @NotNull NodePackage prettierPackage,
+                                         @Nullable TextRange range);
+
+  @NotNull
+  static PrettierLanguageServiceImpl getInstance(@NotNull Project project) {
+    return ((PrettierLanguageServiceImpl)ServiceManager.getService(project, PrettierLanguageService.class));
+  }
+
+  class SupportedFilesInfo {
+    @NotNull
+    public final List<String> fileNames;
+    @NotNull
+    public final List<String> extensions;
+
+    public SupportedFilesInfo(@NotNull List<String> fileNames, @NotNull List<String> extensions) {
+      this.fileNames = fileNames;
+      this.extensions = extensions;
+    }
+  }
+
+  class FormatResult {
+    public static final FormatResult IGNORED = new FormatResult(null, null, true);
+    public final String result;
+    public final String error;
+    public final boolean ignored;
+
+    private FormatResult(String result, String error, boolean ignored) {
+      this.result = result;
+      this.error = error;
+      this.ignored = ignored;
+    }
+
+    public static FormatResult error(String error) {
+      return new FormatResult(null, error, false);
+    }
+
+    public static FormatResult formatted(String result) {
+      return new FormatResult(result, null, false);
+    }
+  }
+}
