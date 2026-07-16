1c3b7e67e2410ad6a289d5ea1d67b23e8b0e9470
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:41:57 +0000

    Initialize 191.4738

---
+++ AngularJS/test/org/angular2/resharper/Angular2CodeCompletionTest.java
@@ -0,0 +1,63 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.angular2.resharper;
+
+import com.intellij.lang.resharper.ReSharperTestUtil;
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.PsiTestUtil;
+import com.intellij.testFramework.TestDataPath;
+import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Set;
+
+@TestDataPath("$R#_COMPLETION_TEST_ROOT/Angular2")
+public class Angular2CodeCompletionTest extends Angular2ReSharperCompletionTestBase {
+
+  private static final Set<String> TESTS_TO_SKIP = ContainerUtil.newHashSet(
+    "external/test002",
+    "external/test003",
+    "external/test004",
+    "external/test005",
+    "external/test006",
+    "test004"
+  );
+
+  @NotNull
+  private VirtualFile getNodeModules() {
+    VirtualFile nodeModules = ReSharperTestUtil.fetchVirtualFile(
+      getTestDataPath(), getBasePath() + "/external/node_modules", getTestRootDisposable());
+    assert nodeModules != null;
+    return nodeModules;
+  }
+
+  @Override
+  protected boolean isExcluded() {
+    return TESTS_TO_SKIP.contains(getName());
+  }
+
+  @Override
+  protected void doSingleTest(@NotNull String testFile, @NotNull String path) throws Exception {
+    if (getName().startsWith("external")) {
+      WriteAction.runAndWait(() -> {
+        VirtualFile nodeModules = getNodeModules();
+        PsiTestUtil.addSourceContentToRoots(myModule, nodeModules);
+        Disposer.register(myFixture.getTestRootDisposable(),
+                          () -> PsiTestUtil.removeContentEntry(myModule, nodeModules));
+      });
+    }
+    myFixture.copyFileToProject("../../package.json", "package.json");
+    super.doSingleTest(testFile, path);
+  }
+
+  @Override
+  protected List<String> doGetExtraFiles() {
+    List<String> extraFiles = super.doGetExtraFiles();
+    if (getName().startsWith("external")) {
+      extraFiles.add("external/module.ts");
+    }
+    return extraFiles;
+  }
+}
