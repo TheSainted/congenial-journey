5067edbab1132b0c3706a653c36b24e9fdcb81b0
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:26:28 +0000

    Initialize on-air

---
+++ AngularJS/src/org/angularjs/refactoring/AngularJSDirectiveRenameProcessor.java
@@ -0,0 +1,75 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.angularjs.refactoring;
+
+import com.intellij.lang.javascript.psi.stubs.JSImplicitElement;
+import com.intellij.lang.javascript.refactoring.JSDefaultRenameProcessor;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.ElementDescriptionLocation;
+import com.intellij.psi.ElementDescriptionProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNamedElement;
+import com.intellij.refactoring.listeners.RefactoringElementListener;
+import com.intellij.refactoring.rename.RenameDialog2;
+import com.intellij.refactoring.rename.RenameUtil;
+import com.intellij.refactoring.rename.ValidationResult;
+import com.intellij.usageView.UsageInfo;
+import com.intellij.usageView.UsageViewTypeLocation;
+import com.intellij.util.IncorrectOperationException;
+import org.angularjs.codeInsight.DirectiveUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+
+/**
+ * @author Dennis.Ushakov
+ */
+public class AngularJSDirectiveRenameProcessor extends JSDefaultRenameProcessor {
+  @Override
+  public boolean canProcessElement(@NotNull PsiElement element) {
+    return DirectiveUtil.getDirective(element) != null;
+  }
+
+  @Nullable
+  @Override
+  public PsiElement substituteElementToRename(@NotNull PsiElement element, @Nullable Editor editor) {
+    return DirectiveUtil.getDirective(element);
+  }
+
+  @Override
+  public void renameElement(@NotNull PsiElement element, @NotNull String newName, @NotNull UsageInfo[] usages, @Nullable RefactoringElementListener listener) throws IncorrectOperationException {
+    final PsiNamedElement directive = (PsiNamedElement)element;
+    final String attributeName = DirectiveUtil.getAttributeName(newName);
+    for (UsageInfo usage : usages) {
+      RenameUtil.rename(usage, attributeName);
+    }
+    directive.setName(DirectiveUtil.attributeToDirective(element, newName));
+    if (listener != null) {
+      listener.elementRenamed(element);
+    }
+  }
+
+  @NotNull
+  @Override
+  public RenameDialog2 createRenameDialog2(@NotNull Project project, @NotNull final PsiElement element, PsiElement nameSuggestionContext, Editor editor) {
+    RenameDialog2 d = super.createRenameDialog2(project, element, nameSuggestionContext, editor);
+    d.setValidate(s -> new ValidationResult(true, null));
+    final String directiveName = DirectiveUtil.attributeToDirective(element, ((PsiNamedElement)element).getName());
+    d.setSuggestedNames(Collections.singletonList(directiveName));
+    return d;
+  }
+
+  public static class AngularJSDirectiveElementDescriptor implements ElementDescriptionProvider {
+    @Nullable
+    @Override
+    public String getElementDescription(@NotNull PsiElement element, @NotNull ElementDescriptionLocation location) {
+      JSImplicitElement directive = DirectiveUtil.getDirective(element);
+      if (directive != null) {
+        if (location instanceof UsageViewTypeLocation) return "directive";
+        return DirectiveUtil.attributeToDirective(directive, directive.getName());
+      }
+      return null;
+    }
+  }
+}
