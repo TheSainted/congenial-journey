31eeaefa9a05343e0eb159fe0af16ac589537bba
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:12:50 +0000

    Initialize kl/undo-rebase

---
+++ tslint/test/data/highlighting/FixSingleError/fix.ts
@@ -0,0 +1,4 @@
+let abc = <error descr="TSLint: ' should be &quot; (quotemark)"><caret>'aaa'</error>;
+console.log(abc);
+
+let cde = <error descr="TSLint: ' should be &quot; (quotemark)">'cde'</error>;
