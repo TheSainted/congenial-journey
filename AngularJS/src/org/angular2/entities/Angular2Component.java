be5f833adbef3027c04989709061de5312c85232
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:18:35 +0000

    Initialize 191.8026

---
+++ AngularJS/src/org/angular2/entities/Angular2Component.java
@@ -0,0 +1,20 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.angular2.entities;
+
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public interface Angular2Component extends Angular2Directive {
+
+  @Nullable
+  PsiFile getTemplateFile();
+
+  @NotNull
+  List<PsiFile> getCssFiles();
+
+  @NotNull
+  List<Angular2DirectiveSelector> getNgContentSelectors();
+}
