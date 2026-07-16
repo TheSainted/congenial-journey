ec5094c8949d02d140d8004f49e0a0b26b298fb4
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:01:41 +0000

    Initialize mitropo/temp-injection-undo

---
+++ tslint/src/com/intellij/lang/javascript/linter/tslint/service/protocol/TsLintLanguageServiceProtocol.kt
@@ -0,0 +1,32 @@
+package com.intellij.lang.javascript.linter.tslint.service.protocol
+
+import com.intellij.lang.javascript.linter.tslint.config.TsLintConfiguration
+import com.intellij.lang.javascript.service.JSLanguageServiceUtil
+import com.intellij.lang.javascript.service.JSLanguageServiceUtil.getPluginDirectory
+import com.intellij.lang.javascript.service.protocol.JSLanguageServiceInitialState
+import com.intellij.lang.javascript.service.protocol.JSLanguageServiceNodeStdProtocolBase
+import com.intellij.openapi.project.Project
+import com.intellij.util.Consumer
+
+
+class TsLintLanguageServiceProtocol(project: Project, readyConsumer: Consumer<*>) : JSLanguageServiceNodeStdProtocolBase(project,
+                                                                                                                         readyConsumer) {
+
+  override fun getNodeInterpreter(): String? {
+    val extendedState = TsLintConfiguration.getInstance(myProject).extendedState
+    return JSLanguageServiceUtil.getInterpreterPathIfValid(extendedState.state.interpreterRef.resolve(myProject))
+  }
+
+  override fun createState(): JSLanguageServiceInitialState {
+    val result = TsLintLanguageServiceInitialState()
+    val extendedState = TsLintConfiguration.getInstance(myProject).extendedState
+    result.tslintPackagePath = extendedState.state.packagePath
+    result.additionalRootDirectory = extendedState.state.rulesDirectory
+    result.pluginName = "tslint"
+    result.pluginPath = getPluginDirectory(this.javaClass, "js/languageService/tslint-plugin-provider.js").absolutePath
+    return result
+  }
+
+  override fun dispose() {
+  }
+}
