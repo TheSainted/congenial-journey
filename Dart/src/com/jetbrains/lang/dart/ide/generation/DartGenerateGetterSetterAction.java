fdae861f4dcfff2ff9403c18034293c540106307
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:21:21 -0700

    Initialize rider/xDebugAsyncExpressionInfoCalculation

---
+++ Dart/src/com/jetbrains/lang/dart/ide/generation/DartGenerateGetterSetterAction.java
@@ -0,0 +1,15 @@
+package com.jetbrains.lang.dart.ide.generation;
+
+import com.jetbrains.lang.dart.DartBundle;
+
+public class DartGenerateGetterSetterAction extends BaseDartGenerateAction {
+  @Override
+  protected BaseDartGenerateHandler getGenerateHandler() {
+    return new DartGenerateAccessorHandler(CreateGetterSetterFix.Strategy.GETTERSETTER) {
+      @Override
+      protected String getTitle() {
+        return DartBundle.message("fields.to.generate.getters.setters");
+      }
+    };
+  }
+}
---
+++ vuejs/src/org/jetbrains/vuejs/language/VueLexer.kt
@@ -0,0 +1,72 @@
+package org.jetbrains.vuejs.language
+
+import com.intellij.lang.HtmlScriptContentProvider
+import com.intellij.lang.Language
+import com.intellij.lexer.HtmlHighlightingLexer
+import com.intellij.lexer.HtmlLexer
+import com.intellij.psi.xml.XmlTokenType
+
+class VueLexer : HtmlLexer(), VueHandledLexer {
+  private var seenTemplate: Boolean = false
+
+  init {
+    registerHandler(XmlTokenType.XML_NAME, VueLangAttributeHandler())
+    registerHandler(XmlTokenType.XML_NAME, VueTemplateTagHandler())
+    val scriptCleaner = VueTemplateCleaner()
+    registerHandler(XmlTokenType.XML_END_TAG_START, scriptCleaner)
+    registerHandler(XmlTokenType.XML_EMPTY_ELEMENT_END, scriptCleaner)
+  }
+
+  override fun findScriptContentProvider(mimeType: String?): HtmlScriptContentProvider? {
+    val type = super.findScriptContentProvider(mimeType ?: "text/ecmascript-6")
+    return type ?: scriptContentViaLang()
+  }
+
+  override fun getStyleLanguage(): Language? {
+    return styleViaLang(ourDefaultStyleLanguage) ?: super.getStyleLanguage()
+  }
+
+  override fun seenScript() = seenScript
+  override fun seenStyle()= seenStyle
+  override fun seenTemplate() = seenTemplate
+  override fun seenTag() = seenTag
+  override fun seenAttribute() = seenAttribute
+  override fun getScriptType() = scriptType
+  override fun getStyleType() = styleType
+  override fun inTagState(): Boolean = isHtmlTagState(state and HtmlHighlightingLexer.BASE_STATE_MASK)
+
+  override fun setSeenScriptType() {
+    seenContentType = true
+  }
+
+  override fun setSeenScript() {
+    seenScript = true
+  }
+
+  override fun setSeenStyleType() {
+    seenStylesheetType = true
+  }
+
+  override fun setSeenTemplate(template:Boolean) {
+    seenTemplate = template
+  }
+
+  override fun setSeenTag(tag: Boolean) {
+    seenTag = tag
+  }
+
+  override fun start(buffer: CharSequence, startOffset: Int, endOffset: Int, initialState: Int) {
+    seenTemplate = initialState and VueTemplateTagHandler.SEEN_TEMPLATE != 0
+    super.start(buffer, startOffset, endOffset, initialState)
+  }
+
+  override fun getState(): Int {
+    val state = super.getState()
+    return state or if (seenTemplate) VueTemplateTagHandler.SEEN_TEMPLATE else 0
+  }
+
+  override fun endOfTheEmbeddment(name:String?):Boolean {
+    return super.endOfTheEmbeddment(name) ||
+           seenTemplate && "template" == name
+  }
+}
