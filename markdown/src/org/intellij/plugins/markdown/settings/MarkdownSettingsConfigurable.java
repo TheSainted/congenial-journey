adc1c7ad2ed6032a1f32ec39dee1b802c01be194
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:05:20 +0000

    Initialize 182-skr-debug

---
+++ markdown/src/org/intellij/plugins/markdown/settings/MarkdownSettingsConfigurable.java
@@ -0,0 +1,101 @@
+package org.intellij.plugins.markdown.settings;
+
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.SearchableConfigurable;
+import com.intellij.openapi.util.Disposer;
+import org.intellij.plugins.markdown.MarkdownBundle;
+import org.intellij.plugins.markdown.ui.preview.MarkdownHtmlPanelProvider;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MarkdownSettingsConfigurable implements SearchableConfigurable {
+  @Nullable
+  private MarkdownSettingsForm myForm = null;
+  @NotNull
+  private final MarkdownApplicationSettings myMarkdownApplicationSettings;
+
+  public MarkdownSettingsConfigurable(@NotNull MarkdownApplicationSettings markdownApplicationSettings) {
+    myMarkdownApplicationSettings = markdownApplicationSettings;
+  }
+
+  @NotNull
+  @Override
+  public String getId() {
+    return "Settings.Markdown";
+  }
+
+  @Nls
+  @Override
+  public String getDisplayName() {
+    return MarkdownBundle.message("markdown.settings.name");
+  }
+
+  @Nullable
+  @Override
+  public JComponent createComponent() {
+    MarkdownSettingsForm form = getForm();
+    if (form == null) {
+      JPanel panel = new JPanel(new BorderLayout());
+      panel.add(new JLabel(MarkdownBundle.message("markdown.settings.no.providers")), BorderLayout.NORTH);
+      return panel;
+    }
+    return form.getComponent();
+  }
+
+  @Nullable
+  public MarkdownSettingsForm getForm() {
+    if (!MarkdownHtmlPanelProvider.hasAvailableProviders()) {
+      return null;
+    }
+
+    if (myForm == null) {
+      myForm = new MarkdownSettingsForm();
+    }
+    return myForm;
+  }
+
+  @Override
+  public boolean isModified() {
+    MarkdownSettingsForm form = getForm();
+    if (form == null) {
+      return false;
+    }
+    return !form.getMarkdownCssSettings().equals(myMarkdownApplicationSettings.getMarkdownCssSettings()) ||
+           !form.getMarkdownPreviewSettings().equals(myMarkdownApplicationSettings.getMarkdownPreviewSettings());
+  }
+
+  @Override
+  public void apply() throws ConfigurationException {
+    final MarkdownSettingsForm form = getForm();
+    if (form == null) {
+      return;
+    }
+
+    form.validate();
+
+    myMarkdownApplicationSettings.setMarkdownCssSettings(form.getMarkdownCssSettings());
+    myMarkdownApplicationSettings.setMarkdownPreviewSettings(form.getMarkdownPreviewSettings());
+  }
+
+  @Override
+  public void reset() {
+    MarkdownSettingsForm form = getForm();
+    if (form == null) {
+      return;
+    }
+    form.setMarkdownCssSettings(myMarkdownApplicationSettings.getMarkdownCssSettings());
+    form.setMarkdownPreviewSettings(myMarkdownApplicationSettings.getMarkdownPreviewSettings());
+  }
+
+  @Override
+  public void disposeUIResources() {
+    if (myForm != null) {
+      Disposer.dispose(myForm);
+    }
+    myForm = null;
+  }
+}
