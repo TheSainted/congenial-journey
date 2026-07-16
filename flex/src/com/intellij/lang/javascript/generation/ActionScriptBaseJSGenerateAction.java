85c374e3dba47f1bf96282351904457ec5a1bcb0
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:59:19 -0700

    Initialize uta/dirtyPsi

---
+++ flex/src/com/intellij/lang/javascript/generation/ActionScriptBaseJSGenerateAction.java
@@ -0,0 +1,18 @@
+package com.intellij.lang.javascript.generation;
+
+import com.intellij.lang.javascript.DialectDetector;
+import com.intellij.lang.javascript.JavaScriptSupportLoader;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+
+public abstract class ActionScriptBaseJSGenerateAction extends BaseJSGenerateAction {
+
+  @Override
+  protected boolean isApplicableForMemberContainer(@NotNull PsiElement jsClass, PsiFile psiFile, @NotNull Editor editor) {
+    return super.isApplicableForMemberContainer(jsClass, psiFile, editor) &&
+           JavaScriptSupportLoader.ECMA_SCRIPT_L4 == DialectDetector.languageOfElement(jsClass);
+  }
+}
---
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/VueXmlExtension.kt
@@ -0,0 +1,57 @@
+package org.jetbrains.vuejs.codeInsight
+
+import com.intellij.lang.ASTNode
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.source.xml.SchemaPrefix
+import com.intellij.psi.impl.source.xml.TagNameReference
+import com.intellij.psi.xml.XmlAttribute
+import com.intellij.psi.xml.XmlTag
+import com.intellij.xml.HtmlXmlExtension
+import org.jetbrains.vuejs.VueLanguage
+
+class VueXmlExtension : HtmlXmlExtension() {
+  override fun isAvailable(file: PsiFile?): Boolean = file?.language is VueLanguage
+
+  override fun getPrefixDeclaration(context: XmlTag, namespacePrefix: String?): SchemaPrefix? {
+    if ("v-bind" == namespacePrefix || "v-on" == namespacePrefix) {
+      val attribute = findAttributeSchema(context, namespacePrefix, 0)
+      if (attribute != null) return attribute
+    }
+    return super.getPrefixDeclaration(context, namespacePrefix)
+  }
+
+  private fun findAttributeSchema(context: XmlTag, namespacePrefix: String, offset: Int): SchemaPrefix? {
+    context.attributes
+      .filter { it.name.startsWith(namespacePrefix) }
+      .forEach { return SchemaPrefix(it, TextRange.create(offset, namespacePrefix.length), namespacePrefix.substring(offset)) }
+    return null
+  }
+
+  override fun isRequiredAttributeImplicitlyPresent(tag: XmlTag?, attrName: String?): Boolean {
+    if (attrName == null) return false
+    val otherCase = if (attrName.contains('-')) toAsset(attrName) else fromAsset(attrName)
+    val predicate: (XmlAttribute) -> Boolean
+    if (otherCase == attrName) {
+      predicate = { it.name == "v-bind:" + attrName || it.name == ":" + attrName || it.name == attrName}
+    } else {
+      predicate = { it.name == "v-bind:" + attrName || it.name == ":" + attrName || it.name == attrName ||
+                    it.name == "v-bind:" + otherCase || it.name == ":" + otherCase || it.name == otherCase }
+    }
+    tag?.attributes?.filter(predicate)?.forEach { return true }
+    return super.isRequiredAttributeImplicitlyPresent(tag, attrName)
+  }
+
+  override fun isCollapsibleTag(tag: XmlTag?): Boolean = VueTagProvider().getDescriptor(tag) != null
+
+  override fun createTagNameReference(nameElement: ASTNode?, startTagFlag: Boolean): TagNameReference? {
+    val parentTag = nameElement?.treeParent as? XmlTag
+    if (parentTag != null) {
+      val descriptor = VueTagProvider().getDescriptor(parentTag)
+      if (descriptor != null) {
+        return VueTagNameReference(nameElement!!, startTagFlag)
+      }
+    }
+    return super.createTagNameReference(nameElement, startTagFlag)
+  }
+}
