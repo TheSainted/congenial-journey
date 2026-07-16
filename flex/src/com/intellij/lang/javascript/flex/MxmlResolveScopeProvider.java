9d8dab3741f9f3a6f1a30a1e0668a6d05c2bb2a1
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:13:23 -0700

    Initialize 163.4396

---
+++ flex/src/com/intellij/lang/javascript/flex/MxmlResolveScopeProvider.java
@@ -0,0 +1,16 @@
+package com.intellij.lang.javascript.flex;
+
+import com.intellij.lang.javascript.JavaScriptSupportLoader;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * User: ksafonov
+ */
+public class MxmlResolveScopeProvider extends ActionScriptResolveScopeProvider {
+
+  @Override
+  protected boolean isApplicable(@NotNull final VirtualFile file) {
+    return JavaScriptSupportLoader.isFlexMxmFile(file);
+  }
+}
