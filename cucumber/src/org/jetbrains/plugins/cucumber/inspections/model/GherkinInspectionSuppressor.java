c8877519feed7965d45d1e9edf19ca540a2ca5b7
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:33:01 +0000

    Initialize 201.6668

---
+++ cucumber/src/org/jetbrains/plugins/cucumber/inspections/model/GherkinInspectionSuppressor.java
@@ -0,0 +1,20 @@
+package org.jetbrains.plugins.cucumber.inspections.model;
+
+import com.intellij.codeInspection.InspectionSuppressor;
+import com.intellij.codeInspection.SuppressQuickFix;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.cucumber.inspections.suppress.GherkinSuppressionUtil;
+
+public class GherkinInspectionSuppressor implements InspectionSuppressor {
+  @Override
+  public boolean isSuppressedFor(@NotNull PsiElement element, @NotNull String toolId) {
+    return GherkinSuppressionUtil.isSuppressedFor(element, toolId);
+  }
+
+  @Override
+  public SuppressQuickFix @NotNull [] getSuppressActions(@Nullable PsiElement element, @NotNull String toolId) {
+    return GherkinSuppressionUtil.getDefaultSuppressActions(toolId);
+  }
+}
