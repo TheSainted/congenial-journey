585a5727726db83a53e86b9465401d1725a9cfca
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:37:31 -0700

    Initialize link/perf_tests

---
+++ vuejs/src/org/jetbrains/vuejs/language/VueTemplateTagHandler.kt
@@ -0,0 +1,20 @@
+package org.jetbrains.vuejs.language
+
+import com.intellij.lexer.BaseHtmlLexer
+import com.intellij.lexer.Lexer
+
+class VueTemplateTagHandler : BaseHtmlLexer.TokenHandler {
+  companion object {
+    val SEEN_TEMPLATE:Int = 0x2000
+  }
+
+  override fun handleElement(lexer: Lexer?) {
+    val handled = lexer as VueHandledLexer
+    if (!handled.seenTag() && handled.inTagState() && "template" == lexer.tokenText) {
+      handled.setSeenTemplate(true)
+    }
+    if (!handled.inTagState() && "template" == lexer.tokenText) {
+      handled.setSeenTag(false)
+    }
+  }
+}
