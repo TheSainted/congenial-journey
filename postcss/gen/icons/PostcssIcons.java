127d296111f060b256ded9da2d79bd3be20114e6
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:37:39 +0000

    Initialize 202-space-plugin

---
+++ postcss/gen/icons/PostcssIcons.java
@@ -0,0 +1,20 @@
+package icons;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class PostcssIcons {
+  private static @NotNull Icon load(@NotNull String path) {
+    return IconManager.getInstance().getIcon(path, PostcssIcons.class);
+  }
+
+  /** 16x16 */ public static final @NotNull Icon Custom_media = load("/org/intellij/plugins/postcss/custom_media.svg");
+  /** 16x16 */ public static final @NotNull Icon Custom_selectors = load("/org/intellij/plugins/postcss/custom_selectors.svg");
+  /** 16x16 */ public static final @NotNull Icon Postcss = load("/org/intellij/plugins/postcss/postcss.svg");
+}
---
+++ prettierJS/src/com/intellij/prettierjs/PrettierCodeStyleEditorNotificationProvider.java
@@ -0,0 +1,89 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.prettierjs;
+
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.lang.javascript.buildTools.npm.PackageJsonUtil;
+import com.intellij.lang.javascript.library.JSLibraryUtil;
+import com.intellij.lang.javascript.psi.util.JSProjectUtil;
+import com.intellij.openapi.editor.colors.EditorColors;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.TextEditor;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.EditorNotificationPanel;
+import com.intellij.ui.EditorNotifications;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public final class PrettierCodeStyleEditorNotificationProvider extends EditorNotifications.Provider<EditorNotificationPanel>
+  implements DumbAware {
+
+  private static final Key<EditorNotificationPanel> KEY = Key.create("prettier.codestyle.notification.panel");
+  private static final String NOTIFICATION_DISMISSED_PROPERTY = "prettier.import.notification.dismissed";
+
+  private final Project myProject;
+
+  public PrettierCodeStyleEditorNotificationProvider(@NotNull Project project) {
+    myProject = project;
+  }
+
+  private boolean isNotificationDismissed(@NotNull VirtualFile file) {
+    return PropertiesComponent.getInstance(myProject).getBoolean(NOTIFICATION_DISMISSED_PROPERTY) ||
+           !PrettierUtil.isConfigFileOrPackageJson(file);
+  }
+
+  private void dismissNotification() {
+    PropertiesComponent.getInstance(myProject).setValue(NOTIFICATION_DISMISSED_PROPERTY, true);
+    EditorNotifications.getInstance(myProject).updateAllNotifications();
+  }
+
+  @NotNull
+  @Override
+  public Key<EditorNotificationPanel> getKey() {
+    return KEY;
+  }
+
+  @Nullable
+  @Override
+  public EditorNotificationPanel createNotificationPanel(@NotNull VirtualFile file,
+                                                         @NotNull FileEditor fileEditor,
+                                                         @NotNull Project project) {
+    if (!(fileEditor instanceof TextEditor)) return null;
+    if (!file.isWritable() || JSProjectUtil.isInLibrary(file, project) || JSLibraryUtil.isProbableLibraryFile(file)) {
+      return null;
+    }
+    if (isNotificationDismissed(file)) {
+      return null;
+    }
+
+    PrettierUtil.Config config = null;
+    if (PrettierUtil.isConfigFile(file)) {
+      config = PrettierUtil.parseConfig(project, file);
+    }
+    if (PackageJsonUtil.isPackageJsonFile(file)) {
+      //if package.json is currently opened, but there is a neighboring config file
+      VirtualFile configVFile = PrettierUtil.findSingleConfigInDirectory(file.getParent());
+      if (configVFile != null) {
+        config = PrettierUtil.parseConfig(project, configVFile);
+      }
+      else {
+        config = PrettierUtil.parseConfig(project, file);
+      }
+    }
+    if (config == null) {
+      return null;
+    }
+    if (config.isInstalled(project)) {
+      return null;
+    }
+    final EditorNotificationPanel panel = new EditorNotificationPanel(EditorColors.GUTTER_BACKGROUND);
+    panel.setText(PrettierBundle.message("editor.notification.title"));
+
+    panel.createActionLabel(PrettierBundle.message("editor.notification.yes.text"), PrettierImportCodeStyleAction.ACTION_ID, false);
+    panel.createActionLabel(PrettierBundle.message("editor.notification.no.text"), () -> dismissNotification(), false);
+
+    return panel;
+  }
+}
