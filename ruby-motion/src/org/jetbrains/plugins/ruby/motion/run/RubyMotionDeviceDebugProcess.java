b2a5af30890d23364c51ad18092c3becec1c4a9b
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-15 20:39:22 -0700

    Initialize 172.3425

---
+++ ruby-motion/src/org/jetbrains/plugins/ruby/motion/run/RubyMotionDeviceDebugProcess.java
@@ -0,0 +1,70 @@
+package org.jetbrains.plugins.ruby.motion.run;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.Executor;
+import com.intellij.execution.configurations.RunProfileState;
+import com.intellij.execution.filters.TextConsoleBuilder;
+import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.util.ArrayUtil;
+import com.intellij.xdebugger.XDebugSession;
+import com.intellij.xdebugger.breakpoints.XBreakpointHandler;
+import com.jetbrains.cidr.execution.debugger.CidrDebuggerLog;
+import com.jetbrains.cidr.execution.debugger.IPhoneDebugProcess;
+import com.jetbrains.cidr.execution.debugger.breakpoints.CidrBreakpointHandler;
+import com.jetbrains.cidr.execution.debugger.breakpoints.CidrExceptionBreakpointHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.ruby.ruby.run.configuration.RubyAbstractCommandLineState;
+
+/**
+ * @author Dennis.Ushakov
+ */
+public class RubyMotionDeviceDebugProcess extends IPhoneDebugProcess {
+  @NotNull private final RunProfileState myState;
+  @NotNull private final Executor myExecutor;
+  @NotNull private final ProcessHandler myProcessHandler;
+
+  public RubyMotionDeviceDebugProcess(@NotNull XDebugSession session,
+                                      @NotNull RunProfileState state,
+                                      @NotNull Executor executor,
+                                      @NotNull TextConsoleBuilder consoleBuilder,
+                                      @NotNull ProcessHandler processHandler) throws ExecutionException {
+    super(new MotionAppRunParameters(true, processHandler), MotionAppRunParameters.getDevice(processHandler), session, consoleBuilder, false);
+    myState = state;
+    myExecutor = executor;
+    myProcessHandler = processHandler;
+  }
+
+  @NotNull
+  @Override
+  protected CidrBreakpointHandler createBreakpointHandler() {
+    return new CidrBreakpointHandler(this, MotionLineBreakpointType.class);
+  }
+
+  @NotNull
+  @Override
+  protected CidrExceptionBreakpointHandler createExceptionHandler() {
+    return new CidrExceptionBreakpointHandler(this, MotionExceptionBreakpointType.class);
+  }
+
+  @NotNull
+  @Override
+  public XBreakpointHandler<?>[] getBreakpointHandlers() {
+    final XBreakpointHandler<?>[] handlers = super.getBreakpointHandlers();
+    return ArrayUtil.remove(handlers, 3);
+  }
+
+  @NotNull
+  @Override
+  public ConsoleView createConsole() {
+    myConsole.attachToProcess(myProcessHandler);
+    try {
+      if (myState instanceof RubyAbstractCommandLineState) {
+        return ((RubyAbstractCommandLineState)myState).createAndAttachConsole(getSession().getProject(), getProcessHandler(), myExecutor);
+      }
+    } catch (ExecutionException e) {
+      CidrDebuggerLog.LOG.info("Error while creating console: " + e);
+    }
+    return myConsole;
+  }
+}
---
+++ tslint/src/com/intellij/lang/javascript/linter/tslint/config/TsLintDescriptor.java
@@ -0,0 +1,53 @@
+package com.intellij.lang.javascript.linter.tslint.config;
+
+import com.intellij.javascript.nodejs.PackageJsonData;
+import com.intellij.lang.javascript.JSBundle;
+import com.intellij.lang.javascript.buildTools.npm.PackageJsonUtil;
+import com.intellij.lang.javascript.linter.JSLinterConfigFileUtil;
+import com.intellij.lang.javascript.linter.JSLinterConfiguration;
+import com.intellij.lang.javascript.linter.JSLinterDescriptor;
+import com.intellij.lang.javascript.linter.tslint.ide.TsLintConfigFileType;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Irina.Chernushina on 11/24/2016.
+ */
+public final class TsLintDescriptor extends JSLinterDescriptor {
+  public static final String PACKAGE_NAME = "tslint";
+
+  @NotNull
+  @Override
+  public String getDisplayName() {
+    return JSBundle.message("settings.javascript.linters.tslint.configurable.name");
+  }
+
+  @Override
+  public String packageName() {
+    return PACKAGE_NAME;
+  }
+
+  @Override
+  public boolean hasConfigFiles(@NotNull Project project) {
+    return JSLinterConfigFileUtil.projectHasConfigFiles(project, TsLintConfigFileType.INSTANCE);
+  }
+
+  @Override
+  public boolean usesLanguageService() {
+    return true;
+  }
+
+  @Override
+  public boolean enable(@NotNull Project project) {
+    final PackageJsonData packageJson = PackageJsonUtil.getTopLevelPackageJsonData(project);
+    // skip if there is tslint-language-service
+    if (packageJson != null && packageJson.getAllDependencies().contains("tslint-language-service")) return false;
+    return super.enable(project);
+  }
+
+  @NotNull
+  @Override
+  public Class<? extends JSLinterConfiguration> getConfigurationClass() {
+    return TsLintConfiguration.class;
+  }
+}
