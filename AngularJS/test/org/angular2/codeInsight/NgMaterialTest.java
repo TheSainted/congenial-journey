9eb7fcccd08efa2402a057bb9476e0f8ca8c0122
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:04:36 +0000

    Initialize viuginick/ruby-debug-inline-variables

---
+++ AngularJS/test/org/angular2/codeInsight/NgMaterialTest.java
@@ -0,0 +1,24 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.angular2.codeInsight;
+
+import com.intellij.codeInspection.htmlInspections.HtmlUnknownAttributeInspection;
+import com.intellij.lang.javascript.inspections.UnterminatedStatementJSInspection;
+import org.angular2.Angular2CodeInsightFixtureTestCase;
+import org.angular2.inspections.AngularUndefinedBindingInspection;
+import org.angularjs.AngularTestUtil;
+
+public class NgMaterialTest extends Angular2CodeInsightFixtureTestCase {
+  @Override
+  protected String getTestDataPath() {
+    return AngularTestUtil.getBaseTestDataPath(getClass()) + "ngMaterial";
+  }
+
+  public void testTemplatesWithSuperConstructors() {
+    myFixture.enableInspections(UnterminatedStatementJSInspection.class,
+                                HtmlUnknownAttributeInspection.class,
+                                AngularUndefinedBindingInspection.class);
+    AngularTestUtil.configureWithMetadataFiles(myFixture, "table", "cdk-index");
+    myFixture.configureByFiles("templateTest.html", "cell.d.ts", "row.ts", "cdk_cell.d.ts", "cdk_row.ts");
+    myFixture.checkHighlighting();
+  }
+}
