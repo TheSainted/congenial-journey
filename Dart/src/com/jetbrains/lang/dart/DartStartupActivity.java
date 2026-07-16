df9f78e20efa561afb0d5141e244bb0e90002620
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 06:39:11 +0000

    Initialize 193

---
+++ Dart/src/com/jetbrains/lang/dart/DartStartupActivity.java
@@ -0,0 +1,108 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.jetbrains.lang.dart;
+
+import com.intellij.ProjectTopics;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.*;
+import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.util.Condition;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.containers.ContainerUtil;
+import com.jetbrains.lang.dart.sdk.DartSdk;
+import gnu.trove.THashSet;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Set;
+
+import static com.jetbrains.lang.dart.util.PubspecYamlUtil.PUBSPEC_YAML;
+
+public class DartStartupActivity implements StartupActivity {
+  @Override
+  public void runActivity(@NotNull Project project) {
+    VirtualFileManager.getInstance().addVirtualFileListener(new DartFileListener(project), project);
+
+    project.getMessageBus().connect().subscribe(ProjectTopics.PROJECT_ROOTS, new ModuleRootListener() {
+      @Override
+      public void rootsChanged(@NotNull ModuleRootEvent event) {
+        DartFileListener.scheduleDartPackageRootsUpdate(project);
+      }
+    });
+
+    final Collection<VirtualFile> pubspecYamlFiles =
+      FilenameIndex.getVirtualFilesByName(project, PUBSPEC_YAML, GlobalSearchScope.projectScope(project));
+
+    for (VirtualFile pubspecYamlFile : pubspecYamlFiles) {
+      final Module module = ModuleUtilCore.findModuleForFile(pubspecYamlFile, project);
+      if (module != null && FileTypeIndex.containsFileOfType(DartFileType.INSTANCE, module.getModuleContentScope())) {
+        excludeBuildAndToolCacheFolders(module, pubspecYamlFile);
+      }
+    }
+
+    if (!pubspecYamlFiles.isEmpty()) {
+      DartFileListener.scheduleDartPackageRootsUpdate(project);
+    }
+  }
+
+  public static void excludeBuildAndToolCacheFolders(final @NotNull Module module, final @NotNull VirtualFile pubspecYamlFile) {
+    final VirtualFile root = pubspecYamlFile.getParent();
+    final VirtualFile contentRoot =
+      root == null ? null : ProjectRootManager.getInstance(module.getProject()).getFileIndex().getContentRootForFile(root);
+    if (contentRoot == null) return;
+
+    // http://pub.dartlang.org/doc/glossary.html#entrypoint-directory
+    // Entrypoint directory: A directory inside your package that is allowed to contain Dart entrypoints.
+    // Pub has a whitelist of these directories: benchmark, bin, example, test, tool, and web.
+    // Any subdirectories of those (except bin) may also contain entrypoints.
+    //
+    // the same can be seen in the pub tool source code: [repo root]/sdk/lib/_internal/pub/lib/src/entrypoint.dart
+
+    final DartSdk sdk = DartSdk.getDartSdk(module.getProject());
+
+    final Collection<String> oldExcludedUrls =
+      ContainerUtil.filter(ModuleRootManager.getInstance(module).getExcludeRootUrls(), new Condition<String>() {
+        final String rootUrl = root.getUrl();
+
+        @Override
+        public boolean value(final String url) {
+          if (url.equals(rootUrl + "/.dart_tool") && sdk != null && StringUtil.compareVersionNumbers(sdk.getVersion(), "2.0") >= 0) {
+            return true;
+          }
+
+          if (url.equals(rootUrl + "/.pub")) return true;
+          if (url.equals(rootUrl + "/build")) return true;
+
+          return false;
+        }
+      });
+
+    final Set<String> newExcludedUrls = collectFolderUrlsToExclude(module, pubspecYamlFile);
+
+    if (oldExcludedUrls.size() != newExcludedUrls.size() || !newExcludedUrls.containsAll(oldExcludedUrls)) {
+      ModuleRootModificationUtil.updateExcludedFolders(module, contentRoot, oldExcludedUrls, newExcludedUrls);
+    }
+  }
+
+  private static Set<String> collectFolderUrlsToExclude(@NotNull final Module module,
+                                                        @NotNull final VirtualFile pubspecYamlFile) {
+    final THashSet<String> newExcludedPackagesUrls = new THashSet<>();
+    final VirtualFile root = pubspecYamlFile.getParent();
+
+    final DartSdk sdk = DartSdk.getDartSdk(module.getProject());
+    if (sdk != null && StringUtil.compareVersionNumbers(sdk.getVersion(), "2.0") >= 0) {
+      newExcludedPackagesUrls.add(root.getUrl() + "/.dart_tool");
+    }
+
+    newExcludedPackagesUrls.add(root.getUrl() + "/.pub");
+    newExcludedPackagesUrls.add(root.getUrl() + "/build");
+
+    return newExcludedPackagesUrls;
+  }
+}
