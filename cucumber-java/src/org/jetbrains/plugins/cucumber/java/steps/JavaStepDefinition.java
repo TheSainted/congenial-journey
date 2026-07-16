2e09d2762a518ac3be64307d792635f8d418ddb1
Kenny <78318799+tpsaint@users.noreply.github.com>
2026-07-16 05:18:32 +0000

    Initialize kkm/errcopy

---
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/steps/JavaStepDefinition.java
@@ -0,0 +1,40 @@
+package org.jetbrains.plugins.cucumber.java.steps;
+
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.plugins.cucumber.ParameterTypeManager;
+import org.jetbrains.plugins.cucumber.java.CucumberJavaUtil;
+
+import static org.jetbrains.plugins.cucumber.CucumberUtil.buildRegexpFromCucumberExpression;
+import static org.jetbrains.plugins.cucumber.java.CucumberJavaUtil.getAllParameterTypes;
+
+public class JavaStepDefinition extends AbstractJavaStepDefinition {
+  private final String myAnnotationClassName;
+
+  public JavaStepDefinition(@NotNull PsiElement stepDef, @NotNull String annotationClassName) {
+    super(stepDef);
+    myAnnotationClassName = annotationClassName;
+  }
+
+  @Nullable
+  @Override
+  protected String getCucumberRegexFromElement(PsiElement element) {
+    if (!(element instanceof PsiMethod)) {
+      return null;
+    }
+    String patternText = CucumberJavaUtil.getStepAnnotationValue((PsiMethod)element, myAnnotationClassName);
+    if (patternText != null &&patternText.length() > 1) {
+      final Module module = ModuleUtilCore.findModuleForPsiElement(element);
+      if (module != null) {
+        ParameterTypeManager parameterTypes = getAllParameterTypes(module);
+        return buildRegexpFromCucumberExpression(patternText.replace("\\\\", "\\").replace("\\\"", "\""), parameterTypes);
+      }
+    }
+
+    return null;
+  }
+}
